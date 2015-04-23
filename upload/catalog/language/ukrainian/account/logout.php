9c469baafd2bec54bdb7a74eab34039c702b8882
ocshop <info@vilkov.info>
2015-04-24 01:28:11 +0300

    catalog\logout.php

--- upload/catalog/language/ukrainian/account/logout.php
+++ upload/catalog/language/ukrainian/account/logout.php
@@ -1,9 +1,14 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title'] = 'Выход';
+$_['heading_title'] = 'Вихід';
 
 // Text
-$_['text_message']  = '<p>Вы вышли из Вашего Личного Кабинета.</p><p>Ваша корзина покупок была сохранена. Она будет восстановлена при следующем входе в Ваш Личный Кабинет.</p>';
-$_['text_account']  = 'Личный Кабинет';
-$_['text_logout']   = 'Выход';
+$_['text_message']  = '<p>Ви вийшли з Вашого Особистого Кабінету.</p><p>Ваш кошик збережен. Він буде відновлен при наступному вході в Ваш Особистий Кабінет.</p>';
+$_['text_account']  = 'Особистий Кабінет';
+$_['text_logout']   = 'Вихід';
 ?>
