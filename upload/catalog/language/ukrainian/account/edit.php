08347442c82d90ee3dd5a6c6aeaf24432185968a
ocshop <info@vilkov.info>
2015-04-24 01:04:29 +0300

    catalog\edit.php

--- upload/catalog/language/ukrainian/account/edit.php
+++ upload/catalog/language/ukrainian/account/edit.php
@@ -1,24 +1,29 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']       = 'Учетная запись';
+$_['heading_title']       = 'Обліковий запис';
 
 // Text
-$_['text_account']        = 'Личный Кабинет';
-$_['text_edit']           = 'Редактировать информацию';
-$_['text_your_details']   = 'Ваша учетная запись';
-$_['text_success']        = 'Ваша учетная запись была успешно обновлена!';
+$_['text_account']        = 'Особистий Кабінет';
+$_['text_edit']           = 'Редагувати інформацію';
+$_['text_your_details']   = 'Ваш обліковий запис';
+$_['text_success']        = 'Ваш обліковий запис був успішно оновлений!';
 
 // Entry
-$_['entry_firstname']     = 'Имя, Отчество:';
-$_['entry_lastname']      = 'Фамилия:';
-$_['entry_email']      = 'E-Mail:';
+$_['entry_firstname']     = 'Ім’я, По батькові:';
+$_['entry_lastname']      = 'Прізвище:';
+$_['entry_email']      	  = 'E-Mail:';
 $_['entry_telephone']     = 'Телефон:';
 $_['entry_fax']           = 'Факс:';
 
 // Error
-$_['error_exists']        = 'Такой E-Mail уже зарегистрирован!';
-$_['error_firstname']     = 'Имя должно быть от 1 до 32 символов!';
-$_['error_lastname']      = 'Фамилия должна быть от 1 до 32 символов!';
-$_['error_email']         = 'E-Mail адрес введен неверно!';
-$_['error_telephone']     = 'Номер телефона должен быть от 3 до 32 символов!';
+$_['error_exists']        = 'Такий E-Mail вже зареєстрований!';
+$_['error_firstname']     = 'Ім’я повинно бути від 1 до 32 символів!';
+$_['error_lastname']      = 'Прізвище повинно бути від 1 до 32 символів!';
+$_['error_email']         = 'E-Mail адресу введено невірно!';
+$_['error_telephone']     = 'Номер телефону повинен бути від 3 до 32 символів!';
 ?>
