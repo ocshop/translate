e864cdb835d32a98445f54df57719b4def9381be
ocshop <info@vilkov.info>
2015-04-24 02:25:33 +0300

    catalog\recurring.php

--- upload/catalog/language/ukrainian/account/recurring.php
+++ upload/catalog/language/ukrainian/account/recurring.php
@@ -1,54 +1,62 @@
 <?php
-$_['heading_title'] = 'Регулярные платежи';
-$_['button_continue'] = 'Продолжить';
-$_['button_view'] = 'Просмотр';
-$_['button_cancel_profile'] = 'Завершить платежи';
-$_['text_empty'] = 'Не найден платежный профиль';
-$_['text_product'] = 'Товар: ';
-$_['text_order'] = 'Заказ: ';
-$_['text_quantity'] = 'Количество: ';
-$_['text_account'] = 'Аккаунт';
-$_['text_action'] = 'Действие';
-$_['text_recurring'] = 'Регулярные платежи';
-$_['text_transactions'] = 'Переводы';
-$_['button_return'] = 'Возврат';
-$_['text_confirm_cancel'] = 'Вы уверены, что хотите отключить свой профиль?';
-$_['text_empty_transactions'] = 'Нет переводов для этого профиля';
 
-$_['column_created'] = 'Создан';
-$_['column_type'] = 'Тип';
-$_['column_amount'] = 'Итого';
-$_['column_status'] = 'Статус';
-$_['column_product'] = 'Товар';
-$_['column_action'] = 'Действие';
-$_['column_profile_id'] = 'ID Профиля';
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
 
-$_['text_recurring_detail'] = 'Детали регулярного платежа';
-$_['text_recurring_id'] = 'ID Профиля: ';
-$_['text_payment_method'] = 'Метод оплаты: ';
-$_['text_date_added'] = 'Создан: ';
-$_['text_recurring_description'] = 'Описание: ';
-$_['text_status'] = 'Статус: ';
-$_['text_ref'] = 'Сноска: ';
+// Heading
+$_['heading_title'] 		  	 			= 'Регулярні платежі';
+$_['button_continue'] 		  	 			= 'Продовжити';
+$_['button_view'] 			  	 			= 'Перегляд';
+$_['button_cancel_profile']   	 			= 'Завершити платежі';
+$_['text_empty'] 			  	 			= 'Не знайдено платіжний профіль';
+$_['text_product'] 			  	 			= 'Товар: ';
+$_['text_order'] 			  	 			= 'Замовлення: ';
+$_['text_quantity'] 		  	 			= 'Кількість: ';
+$_['text_account'] 			  	 			= 'Аккаунт';
+$_['text_action'] 			  	 			= 'Дія';
+$_['text_recurring'] 		  	 			= 'Регулярні платежі';
+$_['text_transactions'] 	  	 			= 'Перекази';
+$_['button_return'] 		  	 			= 'Повернення';
+$_['text_confirm_cancel'] 	  	 			= 'Ви впевнені, що хочете відключити свій профіль?';
+$_['text_empty_transactions'] 	 			= 'Немає переказів для цього профілю';
 
-$_['text_status_active'] = 'Активен';
-$_['text_status_inactive'] = 'Не активен';
-$_['text_status_cancelled'] = 'Отменен';
-$_['text_status_suspended'] = 'Заморожен';
-$_['text_status_expired'] = 'Истек';
-$_['text_status_pending'] = 'Ожидает';
+// Column
+$_['column_created'] 		  	 			= 'Створений';
+$_['column_type'] 			  	 			= 'Тип';
+$_['column_amount'] 		  	 			= 'Всього';
+$_['column_status'] 		  	 			= 'Статус';
+$_['column_product'] 		  	 			= 'Товар';
+$_['column_action'] 		  	 			= 'Дія';
+$_['column_profile_id'] 	  	 			= 'ID Профілю';
 
-$_['text_transaction_created'] = 'Создан';
-$_['text_transaction_payment'] = 'Платеж';
-$_['text_transaction_outstanding_payment'] = 'Непоступивший платеж';
-$_['text_transaction_skipped'] = 'Платеж пропущен';
-$_['text_transaction_failed'] = 'Проблема с оплатой';
-$_['text_transaction_cancelled'] = 'Отменен';
-$_['text_transaction_suspended'] = 'Заморожен';
-$_['text_transaction_suspended_failed'] = 'Заморожен из-за неудачного платежа';
-$_['text_transaction_outstanding_failed'] = 'Платеж не прошел';
-$_['text_transaction_expired'] = 'Истек';
+// Text
+$_['text_recurring_detail']   	 			= 'Деталі регулярного платежу';
+$_['text_recurring_id'] 	  	 			= 'ID Профілю: ';
+$_['text_payment_method'] 	  	 			= 'Метод оплаты: ';
+$_['text_date_added'] 	      	 			= 'Створений: ';
+$_['text_recurring_description'] 			= 'Опис: ';
+$_['text_status'] 				 			= 'Статус: ';
+$_['text_ref'] 					 			= 'Сноска: ';
+$_['text_status_active'] 		 			= 'Активний';
+$_['text_status_inactive'] 		 			= 'Не активний';
+$_['text_status_cancelled'] 	 			= 'Скасовано';
+$_['text_status_suspended'] 	 			= 'Заморожен';
+$_['text_status_expired'] 		 			= 'Протерміновано';
+$_['text_status_pending'] 					= 'Чекає';
+$_['text_transaction_created'] 				= 'Створений';
+$_['text_transaction_payment'] 			    = 'Платіж';
+$_['text_transaction_outstanding_payment']  = 'Невиплачена оплата';
+$_['text_transaction_skipped'] 			    = 'Платіж пропущений';
+$_['text_transaction_failed'] 			    = 'Проблема з оплатою';
+$_['text_transaction_cancelled'] 		    = 'Скасовано';
+$_['text_transaction_suspended'] 		    = 'Заморожен';
+$_['text_transaction_suspended_failed']     = 'Заморожений через невдалий платеж';
+$_['text_transaction_outstanding_failed']   = 'Платіж не пройшов';
+$_['text_transaction_expired'] 			    = 'Протерміновано';
 
-$_['error_not_cancelled'] = 'Ошибка: %s';
-$_['error_not_found'] = 'Нельзя отключить профиль';
-$_['success_cancelled'] = 'Регулярный платеж был закрыт';
+// Error
+$_['error_not_cancelled'] 				    = 'Помилка: %s';
+$_['error_not_found'] 					    = 'Не можна відключити профіль';
+$_['success_cancelled'] 				    = 'Регулярний платіж був закритий';
+?>
