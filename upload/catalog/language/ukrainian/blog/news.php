57696dc7df4a4d9de4e803f2ec70431ff35f5cf5
ocshop <info@vilkov.info>
2015-04-24 17:43:44 +0300

    catalog\news.php

--- upload/catalog/language/ukrainian/blog/news.php
+++ upload/catalog/language/ukrainian/blog/news.php
@@ -1,20 +1,20 @@
 <?php
 // Text
-$_['text_refine']       = 'Выберите подкатегорию';
-$_['text_product']      = 'Статьи';
-$_['text_error']        = 'Категория не найдена!';
-$_['text_empty']        = 'В этой категории нет записей.';
+$_['text_refine']       = 'Оберіть підкатегорію';
+$_['text_product']      = 'Статті';
+$_['text_error']        = 'Категорія не знайдена!';
+$_['text_empty']        = 'У цій категорії немає записів.';
 $_['text_display']      = 'Вид:';
-$_['text_sort']         = 'Сортировка:';
-$_['text_limit']        = 'На странице:';
-$_['text_sort_date']    = 'дате';
+$_['text_sort']         = 'Сортування:';
+$_['text_limit']        = 'На сторінці:';
+$_['text_sort_date']    = 'датою';
 $_['text_list']         = 'списком';
-$_['text_grid']         = 'таблицей';
-$_['text_sort_by']      = 'Сортировать по:';
-$_['text_sort_name']    = 'названию';
-$_['text_sort_rated']   = 'рейтингу';
-$_['text_sort_viewed']  = 'просмотрам';
-$_['text_views'] 	    = 'Просмотров:';
+$_['text_grid']         = 'таблицею';
+$_['text_sort_by']      = 'Сортувати за:';
+$_['text_sort_name']    = 'назвою';
+$_['text_sort_rated']   = 'рейтингом';
+$_['text_sort_viewed']  = 'переглядами';
+$_['text_views'] 	    = 'Переглядів:';
 
-$_['button_more']       = 'подробнее';
+$_['button_more']       = 'детальніше';
 ?>
