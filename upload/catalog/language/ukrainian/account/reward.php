73e9b10e7653800e275fcf52fd959e2f57635409
ocshop <info@vilkov.info>
2015-04-24 14:23:04 +0300

    catalog\reward.php

--- upload/catalog/language/ukrainian/account/reward.php
+++ upload/catalog/language/ukrainian/account/reward.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']      = 'Бонусные баллы';
+$_['heading_title']      = 'Бонусні бали';
 
 // Column
-$_['column_date_added']  = 'Добавлено';
-$_['column_description'] = 'Описание';
-$_['column_points']      = 'Бонусные баллы';
+$_['column_date_added']  = 'Додано';
+$_['column_description'] = 'Опис';
+$_['column_points']      = 'Бонусні бали';
 
 // Text
-$_['text_account']       = 'Личный Кабинет';
-$_['text_reward']        = 'Бонусные баллы';
-$_['text_total']         = 'Накоплено бонусных баллов:';
-$_['text_empty']         = 'У Вас нет бонусных балов!';
+$_['text_account']       = 'Особистий Кабінет';
+$_['text_reward']        = 'Бонусні бали';
+$_['text_total']         = 'Накопилось бонусних балів:';
+$_['text_empty']         = 'У Вас немає бонусних балів!';
 ?>
