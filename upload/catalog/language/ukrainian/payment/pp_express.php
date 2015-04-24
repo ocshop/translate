dbbd9add01a4f85c6b7425c3c69838c1c5dc1c06
ocshop <info@vilkov.info>
2015-04-24 22:19:54 +0300

    catalog/pp_express.php

--- upload/catalog/language/ukrainian/payment/pp_express.php
+++ upload/catalog/language/ukrainian/payment/pp_express.php
@@ -1,4 +1,9 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title'] = 'PayPal Express (включая Кредитные и Дебетовые карты)';
+$_['text_title'] = 'PayPal Express (включаючи кредитні і дебетові карти)';
 ?>
