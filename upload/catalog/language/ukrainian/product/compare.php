71b6027a16d177f4eaf904585ff071498f811c6c
ocshop <info@vilkov.info>
2015-04-24 22:56:38 +0300

    catalog/compare.php

--- upload/catalog/language/ukrainian/product/compare.php
+++ upload/catalog/language/ukrainian/product/compare.php
@@ -1,25 +1,30 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']     = 'Сравнение товаров';
+$_['heading_title']     = 'Порівняння товарів';
 
 // Text
-$_['text_product']      = 'Описание';
+$_['text_product']      = 'Опис';
 $_['text_name']         = 'Товар';
-$_['text_image']        = 'Изображение';
-$_['text_price']        = 'Цена';
+$_['text_image']        = 'Зображення';
+$_['text_price']        = 'Ціна:';
 $_['text_model']        = 'Модель';
-$_['text_manufacturer'] = 'Производитель';
-$_['text_availability'] = 'Наличие';
-$_['text_instock']      = 'Есть на складе';
+$_['text_manufacturer'] = 'Виробник:';
+$_['text_availability'] = 'Наявність';
+$_['text_instock']      = 'Є на складі';
 $_['text_rating']       = 'Рейтинг';
-$_['text_reviews']      = 'На основе %s отзывов.';
-$_['text_summary']      = 'Краткое описание';
-$_['text_weight']       = 'Вес';
-$_['text_dimension']    = 'Размеры (Д х Ш х В)';
-$_['text_compare']      = 'Сравнение товаров (%s)';
-$_['text_success']      = 'Вы добавили <a href="%s">%s</a> в ваш <a href="%s">список сравнения товаров</a>!';
-$_['text_remove']       = 'Товар удален из сравнения';
-$_['text_empty']        = 'Вы не выбрали ни одного товара для сравнения.';
-$_['text_benefits']     = 'Преимущества';
-$_['text_present']      = 'Подарок';
+$_['text_reviews']      = 'На основі %s відгуків.';
+$_['text_summary']      = 'Стислий опис';
+$_['text_weight']       = 'Вага';
+$_['text_dimension']    = 'Розміри (Д х Ш х В)';
+$_['text_compare']      = 'Порівняння товарів (%s)';
+$_['text_success']      = 'Ви додали <a href="%s">%s</a> в ваш <a href="%s">список порівняння товарів</a>!';
+$_['text_remove']       = 'Товар вилучений з порівняння';
+$_['text_empty']        = 'Ви не обрали жодного товару для порівняння.';
+$_['text_benefits']     = 'Переваги';
+$_['text_present']      = 'Подарунок';
 ?>
