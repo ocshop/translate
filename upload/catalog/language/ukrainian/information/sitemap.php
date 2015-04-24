38a522ece8df1b128ea5df57da9b5712156045dc
ocshop <info@vilkov.info>
2015-04-24 19:50:09 +0300

    catalog\sitemap.php

--- upload/catalog/language/ukrainian/information/sitemap.php
+++ upload/catalog/language/ukrainian/information/sitemap.php
@@ -1,19 +1,24 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Карта сайта';
+$_['heading_title']    = 'Мапа сайту';
 
 // Text
-$_['text_special']     = 'Акции';
-$_['text_account']     = 'Личный кабинет';
-$_['text_edit']        = 'Личная информация';
+$_['text_special']     = 'Акції';
+$_['text_account']     = 'Особистий кабінет';
+$_['text_edit']        = 'Особиста інформація';
 $_['text_password']    = 'Пароль';
-$_['text_address']     = 'Мои адреса';
-$_['text_history']     = 'История заказов';
-$_['text_download']    = 'Файлы для скачивания';
+$_['text_address']     = 'Мої адреси';
+$_['text_history']     = 'Історія замовлень';
+$_['text_download']    = 'Файли для скачування';
 $_['text_cart']        = 'Корзина';
-$_['text_checkout']    = 'Оформление заказа';
-$_['text_search']      = 'Поиск';
-$_['text_information'] = 'Информация';
-$_['text_contact']     = 'Наши контакты';
+$_['text_checkout']    = 'Оформлення замовлення';
+$_['text_search']      = 'Пошук';
+$_['text_information'] = 'Інформація';
+$_['text_contact']     = 'Наші контакти';
 $_['text_blog']        = 'Блог';
 ?>
