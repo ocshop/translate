833716f3e810dc9c8693d84d10336cdde3cde422
ocshop <info@vilkov.info>
2015-04-24 16:31:56 +0300

    catalog\payment.php

--- upload/catalog/language/ukrainian/affiliate/payment.php
+++ upload/catalog/language/ukrainian/affiliate/payment.php
@@ -1,25 +1,30 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']             = 'Способ оплаты';
+$_['heading_title']             = 'Спосіб оплати';
 
 // Text
-$_['text_account']              = 'Кабинет Партнера';
+$_['text_account']              = 'Кабінет Партнера';
 $_['text_payment']              = 'Оплата';
-$_['text_your_payment']         = 'Ваши перечисления';
+$_['text_your_payment']         = 'Ваші перерахування';
 $_['text_your_password']        = 'Пароль';
 $_['text_cheque']               = 'Чек';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Банковский перевод';
-$_['text_success']              = 'Учетные данные Партнера успешно обновлены.';
+$_['text_bank']                 = 'Банківський переказ';
+$_['text_success']              = 'Облікові дані Партнера успішно оновлені.';
 
 // Entry
-$_['entry_tax']                 = 'Налоговый код:';
-$_['entry_payment']             = 'Способ оплаты:';
-$_['entry_cheque']              = 'Чек, Имя получателя платежа:';
-$_['entry_paypal']              = 'PayPal Email аккаунт:';
-$_['entry_bank_name']           = 'Название банка:';
-$_['entry_bank_branch_number']  = 'ABA/BSB номер (номер отделения):';
+$_['entry_tax']                 = 'Податковий код:';
+$_['entry_payment']             = 'Спосіб оплати:';
+$_['entry_cheque']              = 'Чек, Ім’я одержувача платежу:';
+$_['entry_paypal']              = 'PayPal E-Mail аккаунт:';
+$_['entry_bank_name']           = 'Назва банку:';
+$_['entry_bank_branch_number']  = 'ABA/BSB номер (номер відділення):';
 $_['entry_bank_swift_code']     = 'SWIFT код:';
-$_['entry_bank_account_name']   = 'Название счёта:';
-$_['entry_bank_account_number'] = 'Номер счёта:';
+$_['entry_bank_account_name']   = 'Назва рахунку:';
+$_['entry_bank_account_number'] = 'Номер рахунку:';
 ?>
