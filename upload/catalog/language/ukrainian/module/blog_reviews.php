cc5fe04aa50803c6b393d941a3b79731529185d1
ocshop <info@vilkov.info>
2015-04-24 21:40:33 +0300

    catalog/blog_reviews.php

--- upload/catalog/language/ukrainian/module/blog_reviews.php
+++ upload/catalog/language/ukrainian/module/blog_reviews.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_all_reviews']  = 'Все отзывы';
-$_['text_views'] 	    = 'Просмотров:';
+$_['text_all_reviews']  = 'Всі відгуки';
+$_['text_views'] 		= 'Переглядів:';
 ?>
