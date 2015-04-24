edff2106b08f3265e400c79116f92dd2f30e9b67
ocshop <info@vilkov.info>
2015-04-24 16:24:59 +0300

    catalog\logout.php

--- upload/catalog/language/ukrainian/affiliate/logout.php
+++ upload/catalog/language/ukrainian/affiliate/logout.php
@@ -1,9 +1,14 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title'] = 'Выход из Кабинета Партнера';
+$_['heading_title'] = 'Вихід з Кабінету Партнера';
 
 // Text
-$_['text_message']  = '<p>Вы вышли из Кабинета Партнера.</p>';
-$_['text_account']  = 'Кабинет Партнера';
-$_['text_logout']   = 'Выход';
+$_['text_message']  = '<p>Ви вийшли з Кабінету Партнера.</p>';
+$_['text_account']  = 'Кабінет Партнера';
+$_['text_logout']   = 'Вихід';
 ?>
