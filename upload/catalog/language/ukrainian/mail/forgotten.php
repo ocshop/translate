26216e56695ff4014f1e6715a1a149f7b1ef978a
ocshop <info@vilkov.info>
2015-04-24 20:25:55 +0300

    catalog\forgotten.php

--- upload/catalog/language/ukrainian/mail/forgotten.php
+++ upload/catalog/language/ukrainian/mail/forgotten.php
@@ -1,6 +1,11 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_subject']  = '%s - новый пароль';
-$_['text_greeting'] = 'Новый пароль был запрошен от %s.';
-$_['text_password'] = 'Ваш новый пароль:';
+$_['text_subject']  = '%s - новий пароль';
+$_['text_greeting'] = 'Новий пароль був запитаний від %s.';
+$_['text_password'] = 'Ваш новий пароль:';
 ?>
