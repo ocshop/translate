0dfe8f8c8f6a4eb3ca5d3931419f26f10be4aa25
ocshop <info@vilkov.info>
2015-04-24 21:29:41 +0300

    catalog\account.php

--- upload/catalog/language/ukrainian/module/account.php
+++ upload/catalog/language/ukrainian/module/account.php
@@ -1,21 +1,26 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Личный кабинет';
+$_['heading_title']    = 'Особистий кабінет';
 
 // Text
-$_['text_register']    = 'Регистрация';
-$_['text_login']       = 'Вход';
-$_['text_logout']      = 'Выход';
-$_['text_forgotten']   = 'Забыли пароль?';
-$_['text_account']     = 'Моя информация';
-$_['text_edit']        = 'Изменить контактную информацию';
+$_['text_register']    = 'Реєстрація';
+$_['text_login']       = 'Вхід';
+$_['text_logout']      = 'Вихід';
+$_['text_forgotten']   = 'Забули пароль?';
+$_['text_account']     = 'Моя інформація';
+$_['text_edit']        = 'Змінити контактну інформацію';
 $_['text_password']    = 'Пароль';
-$_['text_address']     = 'Список контактов';
+$_['text_address']     = 'Список контактів';
 $_['text_wishlist']    = 'Закладки';
-$_['text_order']       = 'История заказов';
-$_['text_download']    = 'Файлы для скачивания';
-$_['text_return']      = 'Возвраты';
-$_['text_transaction'] = 'История фин. операций';
-$_['text_newsletter']  = 'E-Mail рассылка';
-$_['text_recurring']   = 'Регулярные платежи';
+$_['text_order']       = 'Історія замовлень';
+$_['text_download']    = 'Файли для скачування';
+$_['text_return']      = 'Повернення';
+$_['text_transaction'] = 'Історія фін. операцій';
+$_['text_newsletter']  = 'E-Mail розсилка';
+$_['text_recurring']   = 'Регулярні платежі';
 ?>
