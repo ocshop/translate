fc22bd6c2f0b5fb6c8061c09dac1c857deabf394
ocshop <info@vilkov.info>
2015-04-24 22:07:56 +0300

    catalog/cod.php

--- upload/catalog/language/ukrainian/payment/cod.php
+++ upload/catalog/language/ukrainian/payment/cod.php
@@ -1,4 +1,9 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title'] = 'Оплата при доставке';
+$_['text_title'] = 'Оплата при доставці';
 ?>
