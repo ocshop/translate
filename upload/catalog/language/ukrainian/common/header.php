632709f8290c5e23f90e1eb4c680341ea7227129
ocshop <info@vilkov.info>
2015-04-24 19:35:42 +0300

    catalog\header.php

--- upload/catalog/language/ukrainian/common/header.php
+++ upload/catalog/language/ukrainian/common/header.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_home']     = 'Главная';
+$_['text_home']     = 'Головна';
 $_['text_wishlist'] = 'Закладки (%s)';
 $_['text_shopping_cart']  = 'Корзина покупок';
-$_['text_search']   = 'Поиск';
-$_['text_welcome']  = '<a href="%s"><i class="fa fa-user"></i>Войти</a> или <a href="%s"><i class="fa fa-pencil"></i>зарегистрироваться</a>';
-$_['text_logged']   = 'Вы вошли как <i class="fa fa-user"></i> <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b>';
-$_['text_account']  = 'Постоянный покупатель';
-$_['text_checkout'] = 'Оформление заказа';
+$_['text_search']   = 'Пошук';
+$_['text_welcome']  = '<a href="%s"><i class="fa fa-user"></i>Вхід</a> або <a href="%s"><i class="fa fa-pencil"></i>зареєструватися</a>';
+$_['text_logged']   = 'Ви увійшли як <i class="fa fa-user"></i> <a href="%s">%s</a> <b>(</b> <a href="%s">Вихід</a> <b>)</b>';
+$_['text_account']  = 'Постійний покупець';
+$_['text_checkout'] = 'Оформлення замовлення';
 $_['text_blog']	    = 'Блог';
-$_['text_special']  = 'Акции';
+$_['text_special']  = 'Акції';
 $_['text_latest']   = 'Новинки';
-$_['text_brands']   = 'Производители';
+$_['text_brands']   = 'Виробники';
 ?>
