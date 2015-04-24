4f04c27d4e1548a0f5be78ea3dcef68beee3b956
ocshop <info@vilkov.info>
2015-04-24 21:42:46 +0300

    catalog/cart.php

--- upload/catalog/language/ukrainian/module/cart.php
+++ upload/catalog/language/ukrainian/module/cart.php
@@ -1,10 +1,15 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading 
 $_['heading_title'] = 'Корзина покупок';
 
 // Text
-$_['text_items']    = 'Товаров: %s (%s)';
-$_['text_empty']    = 'Ничего не куплено!';
-$_['text_cart']     = 'Просмотр корзины';
-$_['text_checkout'] = 'Оформление заказа';
+$_['text_items']    = 'Товарів: %s (%s)';
+$_['text_empty']    = 'Нічого не куплено!';
+$_['text_cart']     = 'Переглянути корзину';
+$_['text_checkout'] = 'Оформлення замовлення';
 ?>
