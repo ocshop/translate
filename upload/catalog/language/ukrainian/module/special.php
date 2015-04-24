dc148d6235cd2817e304f3e5f136b57d60840737
ocshop <info@vilkov.info>
2015-04-24 21:58:00 +0300

    catalog/special.php

--- upload/catalog/language/ukrainian/module/special.php
+++ upload/catalog/language/ukrainian/module/special.php
@@ -1,7 +1,12 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title'] = 'Акции';
+$_['heading_title'] = 'Акції';
 
 // Text
-$_['text_reviews']  = 'На основании %s отзывов.';
+$_['text_reviews']  = 'На підставі %s відгуків.';
 ?>
