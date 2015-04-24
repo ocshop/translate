fbd343c68c7b7428894252b63492bd958c7ef5f9
ocshop <info@vilkov.info>
2015-04-24 21:48:10 +0300

    catalog/featuredwb.php

--- upload/catalog/language/ukrainian/module/featuredwb.php
+++ upload/catalog/language/ukrainian/module/featuredwb.php
@@ -1,7 +1,11 @@
 <?php
-// Heading 
-$_['heading_title']	= 'Рекомендуем';
 
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+// Heading
+$_['heading_title'] = 'Рекомендуємо';
 // Text
-$_['text_reviews']	= 'На основании %s отзывов.'; 
+$_['text_reviews']  = 'На підставі %s відгуків.';
 ?>
