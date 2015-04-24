7fb0131be6b159a2a38263cdf62e0d9dd18419fb
ocshop <info@vilkov.info>
2015-04-24 16:03:13 +0300

    catalog\account.php

--- upload/catalog/language/ukrainian/affiliate/account.php
+++ upload/catalog/language/ukrainian/affiliate/account.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']        = 'Партнерcкий раздел';
+$_['heading_title']        = 'Партнерський розділ';
 
 // Text
-$_['text_account']         = 'Кабинет Партнера';
-$_['text_my_account']      = 'Моя учетная запись';
-$_['text_my_tracking']     = 'Мои рефералы';
-$_['text_my_transactions'] = 'История выплат';
-$_['text_edit']            = 'Редактировать учетную запись';
-$_['text_password']        = 'Изменить пароль';
-$_['text_payment']         = 'Изменить платежные реквизиты';
-$_['text_tracking']        = 'Реферальный код';
-$_['text_transaction']     = 'Посмотреть историю выплат';
+$_['text_account']         = 'Кабінет Партнера';
+$_['text_my_account']      = 'Мій обліковий запис';
+$_['text_my_tracking']     = 'Мої реферали';
+$_['text_my_transactions'] = 'Історія виплат';
+$_['text_edit']            = 'Змінити контактну інформацію';
+$_['text_password']        = 'Змінити свій пароль';
+$_['text_payment']         = 'Змінити платіжні реквізити';
+$_['text_tracking']        = 'Реферальний код';
+$_['text_transaction']     = 'Подивитися історію виплат';
 ?>
