b487101ecbb93280bf8f1ef160c263a301455956
ocshop <info@vilkov.info>
2015-04-24 17:18:40 +0300

    catalog\register.php

--- upload/catalog/language/ukrainian/affiliate/register.php
+++ upload/catalog/language/ukrainian/affiliate/register.php
@@ -1,57 +1,64 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']             = 'Партнерская программа';
+$_['heading_title']             = 'Партнерська програма';
 
 // Text
-$_['text_account']              = 'Кабинет Партнера';
-$_['text_register']             = 'Регистрация Партнера';
-$_['text_account_already']      = 'Если у вас уже есть партнерский логин, пожалуйста <a href="%s">Войдите</a>.';
-$_['text_signup']               = 'Для создания учетной записи партнера необходимо заполнить форму регистрации:';
-$_['text_your_details']         = 'Ваши персональные данные';
-$_['text_your_address']         = 'Ваш адрес';
-$_['text_payment']              = 'Платёжные реквизиты';
+$_['text_account']              = 'Кабінет Партнера';
+$_['text_register']             = 'Реєстрація Партнера';
+$_['text_account_already']      = 'Якщо у вас вже є партнерський логін, будь ласка перейдіть на сторінку <a href="%s">входу в систему</a>.';
+$_['text_signup']               = 'Для створення облікового запису партнера необхідно заповнити форму реєстрації:';
+$_['text_your_details']         = 'Ваші персональні дані';
+$_['text_your_address']         = 'Ваша адреса';
+$_['text_payment']              = 'Платіжні реквізити';
 $_['text_your_password']        = 'Пароль';
 $_['text_cheque']               = 'Чек';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Банковский перевод';
-$_['text_agree']                = 'Я прочитал <a class="colorbox" href="%s" alt="%s"><b>%s</b></a> и согласен с условиями';
+$_['text_bank']                 = 'Банківський переказ';
+$_['text_agree']                = 'Я прочитав <a class="colorbox" href="%s" alt="%s"><b>%s</b></a> і згоден з умовами';
+
 // Entry
-$_['entry_firstname']           = 'Имя, Отчество:';
-$_['entry_lastname']            = 'Фамилия:';
+$_['entry_firstname']           = 'Ім’я, По батькові:';
+$_['entry_lastname']            = 'Прізвище:';
 $_['entry_email']               = 'E-Mail:';
 $_['entry_telephone']           = 'Телефон:';
 $_['entry_fax']                 = 'Факс:';
-$_['entry_company']             = 'Компания:';
+$_['entry_company']             = 'Компанія:';
 $_['entry_website']             = 'Веб-сайт:';
 $_['entry_address_1']           = 'Адрес:';
-$_['entry_address_2']           = 'Адрес (продолжение):';
-$_['entry_postcode']            = 'Индекс:';
-$_['entry_city']                = 'Город:';
-$_['entry_country']             = 'Страна:';
-$_['entry_zone']                = 'Регион / Область:';
-$_['entry_tax']                 = 'Налоговый код (ИНН):';
-$_['entry_payment']             = 'Способ оплаты:';
-$_['entry_cheque']              = 'Чек, Имя получателя платежа:';
-$_['entry_paypal']              = 'PayPal Email аккаунт:';
-$_['entry_bank_name']           = 'Наименование банка:';
-$_['entry_bank_branch_number']  = 'ABA/BSB номер (номер отделения):';
+$_['entry_address_2']           = 'Адрес (продовження):';
+$_['entry_postcode']            = 'Індекс:';
+$_['entry_city']                = 'Місто:';
+$_['entry_country']             = 'Країна:';
+$_['entry_zone']                = 'Регіон / Область:';
+$_['entry_tax']                 = 'Податковий код (ИНН):';
+$_['entry_payment']             = 'Спосіб оплати:';
+$_['entry_cheque']              = 'Чек, Ім’я одержувача платежу:';
+$_['entry_paypal']              = 'PayPal E-Mail аккаунт:';
+$_['entry_bank_name']           = 'Назва банку:';
+$_['entry_bank_branch_number']  = 'ABA/BSB номер (номер відділення):';
 $_['entry_bank_swift_code']     = 'SWIFT код:';
-$_['entry_bank_account_name']   = 'Имя счета:';
-$_['entry_bank_account_number'] = 'Номер счета:';
+$_['entry_bank_account_name']   = 'Назва рахунку:';
+$_['entry_bank_account_number'] = 'Номер рахунку:';
 $_['entry_password']            = 'Пароль:';
-$_['entry_confirm']             = 'Подтверждение пароля:';
+$_['entry_confirm']             = 'Підтвердження пароля:';
+
 // Error
-$_['error_exists']              = 'Такой E-Mail уже зарегистрирован!';
-$_['error_firstname']           = 'Имя должно быть от 1 до 32 символов!';
-$_['error_lastname']            = 'Фамилия должна быть от 1 до 32 символов!';
-$_['error_email']               = 'E-Mail адрес введен неверно!';
-$_['error_telephone']           = 'Номер телефона должен быть от 3 до 32 символов!';
-$_['error_password']            = 'Пароль должен быть от 4 до 20 символов!';
-$_['error_confirm']             = 'Пароли не совпадают!';
-$_['error_address_1']           = 'Адрес должен быть от 3 до 128 символов!';
-$_['error_city']           		= 'Название города должно быть от 2 до 128 символов!';
-$_['error_country']        		= 'Пожалуйста, выберите страну!';
-$_['error_zone']           		= 'Пожалуйста, выберите регион / область!';
-$_['error_postcode']       		= 'Индекс должен быть от 2 до 10 символов!';
-$_['error_agree']               = 'Вы должны быть согласны с условиями %s!';
+$_['error_exists']              = 'Цей E-Mail вже зареєстрований!';
+$_['error_firstname']           = 'Ім’я повинно бути від 1 до 32 символів!';
+$_['error_lastname']            = 'Прізвище повинно бути від 1 до 32 символів!';
+$_['error_email']               = 'E-Mail введено невірно!';
+$_['error_telephone']           = 'Номер телефону повинен бути від 3 до 32 символів!';
+$_['error_password']            = 'Пароль повинен бути від 4 до 20 символів!';
+$_['error_confirm']             = 'Паролі не співпадають!';
+$_['error_address_1']           = 'Адреса повинна бути від 3 до 128 символів!';
+$_['error_city']           		= 'Назва міста має бути від 2 до 128 символів!';
+$_['error_country']        		= 'Будь ласка, вкажіть країну!';
+$_['error_zone']           		= 'Будь ласка, вкажіть регіон / область!';
+$_['error_postcode']       		= 'Індекс повинен бути від 2 до 10 символів!';
+$_['error_agree']               = 'Ви повинні бути згодні з умовами %s!';
 ?>
