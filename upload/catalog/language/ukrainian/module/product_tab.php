6c6ac6106cc38e657aa17148f3e9f1162eea68fa
ocshop <info@vilkov.info>
2015-04-24 21:56:34 +0300

    catalog/product_tab.php

--- upload/catalog/language/ukrainian/module/product_tab.php
+++ upload/catalog/language/ukrainian/module/product_tab.php
@@ -1,12 +1,17 @@
 <?php
-// Heading 
-$_['heading_title'] = 'Товары';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+// Heading
+$_['heading_title'] = 'Товари';
 
 $_['tab_latest'] = 'Новинки';
-$_['tab_bestseller'] = 'Лидеры продаж';
-$_['tab_featured'] = 'Рекомендуем';
-$_['tab_special'] = 'Акции';
+$_['tab_bestseller'] = 'Лідери продажу';
+$_['tab_featured'] = 'Рекомендуємо';
+$_['tab_special'] = 'Акції';
 
 // Text
-$_['text_reviews']  = 'На основе %s отзывов.'; 
+$_['text_reviews']  = 'На підставі %s відгуків.';
 ?>
