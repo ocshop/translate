ba4090620a24cb97962ec7406150fd55eb79fa53
ocshop <info@vilkov.info>
2015-04-24 19:31:36 +0300

    catalog\footer.php

--- upload/catalog/language/ukrainian/common/footer.php
+++ upload/catalog/language/ukrainian/common/footer.php
@@ -1,23 +1,28 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_information']  = 'Информация';
-$_['text_service']      = 'Служба поддержки';
-$_['text_extra']        = 'Дополнительно';
-$_['text_contact']      = 'Связаться с нами';
-$_['text_abuses']       = 'Пожаловаться Директору';
-$_['text_return']       = 'Возврат товара';
-$_['text_testimonial']  = 'Отзывы о Магазине';
-$_['text_sitemap']      = 'Карта сайта';
-$_['text_manufacturer'] = 'Производители';
-$_['text_voucher']      = 'Подарочные сертификаты';
-$_['text_affiliate']    = 'Партнёрская программа';
-$_['text_special']      = 'Акции';
-$_['text_bestseller']   = 'Хиты Продаж';
-$_['text_mostviewed']   = 'Популярные Товары';
+$_['text_information']  = 'Інформація';
+$_['text_service']      = 'Служба підтримки';
+$_['text_extra']        = 'Додатково';
+$_['text_contact']      = 'Зв’язатися з нами';
+$_['text_abuses']       = 'Поскаржитись Директору';
+$_['text_return']       = 'Повернення товару';
+$_['text_testimonial']  = 'Відгуки про Магазин';
+$_['text_sitemap']      = 'Мапа сайту';
+$_['text_manufacturer'] = 'Виробники';
+$_['text_voucher']      = 'Подарункові сертифікати';
+$_['text_affiliate']    = 'Партнерська програма';
+$_['text_special']      = 'Акції';
+$_['text_bestseller']   = 'Хіти Продажу';
+$_['text_mostviewed']   = 'Популярні Товари';
 $_['text_latest']       = 'Новинки';
-$_['text_account']      = 'Личный Кабинет';
-$_['text_order']        = 'История заказов';
+$_['text_account']      = 'Особистий Кабінет';
+$_['text_order']        = 'Історія замовлень';
 $_['text_wishlist']     = 'Закладки';
-$_['text_newsletter']   = 'Рассылка';
-$_['text_powered']      = '%s &copy; %s<br />Работает на <a href="http://ocshop.info/">OCSHOP.CMS</a>';
+$_['text_newsletter']   = 'Розсилка';
+$_['text_powered']      = '%s &copy; %s<br />Працює на <a href="http://ocshop.info/">OCSHOP.CMS</a>';
 ?>
