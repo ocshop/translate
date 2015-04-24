96666061cd03eb15b8559f8aecd96604410b8efd
ocshop <info@vilkov.info>
2015-04-24 21:44:57 +0300

    category\coolfilter.php

--- upload/catalog/language/ukrainian/module/coolfilter.php
+++ upload/catalog/language/ukrainian/module/coolfilter.php
@@ -1,6 +1,11 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading 
-$_['heading_title']  = 'Фильтр товаров';
-$_['text_apply'] = 'Применить';
-$_['text_reset_coolfilter'] = 'Сбросить фильтр';
+$_['heading_title']  = 'Фільтр товарів';
+$_['text_apply'] = 'Застосувати';
+$_['text_reset_coolfilter'] = 'Скинути фільтр';
 ?>
