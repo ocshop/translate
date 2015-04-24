1bb938f4511795213e607ce472c7c83c7e0aa0c9
ocshop <info@vilkov.info>
2015-04-24 21:49:05 +0300

    catalog/filter.php

--- upload/catalog/language/ukrainian/module/filter.php
+++ upload/catalog/language/ukrainian/module/filter.php
@@ -1,4 +1,9 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title'] = 'Уточнение поиска';
+$_['heading_title'] = 'Уточнення пошуку';
 ?>
