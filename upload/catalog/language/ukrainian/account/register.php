d4d2c7eae2789f772894daf04ea007acd7506ebb
ocshop <info@vilkov.info>
2015-04-24 13:47:42 +0300

    catalog\register.php

--- upload/catalog/language/ukrainian/account/register.php
+++ upload/catalog/language/ukrainian/account/register.php
@@ -1,52 +1,57 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']        = 'Регистрация';
+$_['heading_title']        = 'Реєстрація';
 
 // Text
-$_['text_account']         = 'Личный Кабинет';
-$_['text_register']        = 'Регистрация';
-$_['text_account_already'] = 'Если Вы уже зарегистрированы, перейдите на страницу <a href="%s">входа в систему</a>.';
-$_['text_your_details']    = 'Основные данные';
-$_['text_your_address']    = 'Ваш адрес';
-$_['text_newsletter']      = 'Рассылка новостей';
+$_['text_account']         = 'Особистий Кабінет';
+$_['text_register']        = 'Реєстрація';
+$_['text_account_already'] = 'Якщо Ви вже зареєстровані, перейдіть на сторінку <a href="%s">входу в систему</a>.';
+$_['text_your_details']    = 'Основні дані';
+$_['text_your_address']    = 'Ваша адреса';
+$_['text_newsletter']      = 'Розсилка новин';
 $_['text_your_password']   = 'Ваш пароль';
-$_['text_agree']           = 'Я прочитал <a class="colorbox" href="%s" alt="%s"><b>%s</b></a> и согласен с условиями';
+$_['text_agree']           = 'Я прочитав <a class="colorbox" href="%s" alt="%s"><b>%s</b></a> і згоден з умовами';
 
 // Entry
-$_['entry_firstname']      = 'Имя:';
-$_['entry_lastname']       = 'Фамилия:';
+$_['entry_firstname']      = 'Ім’я:';
+$_['entry_lastname']       = 'Прізвище:';
 $_['entry_email']          = 'E-Mail:';
 $_['entry_telephone']      = 'Телефон:';
 $_['entry_fax']            = 'Факс:';
-$_['entry_company']        = 'Компания:';
-$_['entry_customer_group'] = 'Группа покупателей:';
-$_['entry_company_id']     = 'Регистрационный номер компании:';
-$_['entry_tax_id']         = 'Налоговый номер:';
-$_['entry_address_1']      = 'Адрес 1:';
-$_['entry_address_2']      = 'Адрес 2:';
-$_['entry_postcode']       = 'Индекс:';
-$_['entry_city']           = 'Город:';
-$_['entry_country']        = 'Страна:';
-$_['entry_zone']           = 'Регион / Область:';
-$_['entry_newsletter']     = 'Подписка на новости:';
+$_['entry_company']        = 'Компанія:';
+$_['entry_customer_group'] = 'Група покупців:';
+$_['entry_company_id']     = 'Реєстраційний номер компанії:';
+$_['entry_tax_id']         = 'Податковий номер:';
+$_['entry_address_1']      = 'Адреса 1:';
+$_['entry_address_2']      = 'Адреса 2:';
+$_['entry_postcode']       = 'Індекс:';
+$_['entry_city']           = 'Місто:';
+$_['entry_country']        = 'Країна:';
+$_['entry_zone']           = 'Регіон / Область:';
+$_['entry_newsletter']     = 'Підписка на новини:';
 $_['entry_password']       = 'Пароль:';
-$_['entry_confirm']        = 'Подтверждение пароля:';
+$_['entry_confirm']        = 'Підтвердження пароля:';
 
 // Error
-$_['error_exists']         = 'Этот E-Mail уже зарегистрирован!';
-$_['error_firstname']      = 'Имя должно содержать от 1 до 32 символов!';
-$_['error_lastname']       = 'Фамилия должна содержать от 1 до 32 символов!';
-$_['error_email']          = 'E-Mail введён неправильно!';
-$_['error_telephone']      = 'В телефоне должно быть от 3 до 32 цифр!';
-$_['error_password']       = 'В пароле должно быть от 4 до 20 символов!';
-$_['error_confirm']        = 'Пароли не совпадают!';
-$_['error_company_id']     = 'Укажите рег. номер компании!';
-$_['error_tax_id']         = 'Укажите налоговый номер!';
-$_['error_vat']            = 'Непправильный VAT number!';
-$_['error_address_1']      = 'Адрес должен содержать от 3 до 128 символов!';
-$_['error_city']           = 'Название города должно содержать от 2 до 128 символов!';
-$_['error_postcode']       = 'В индексе должно быть от 2 до 10 символов!';
-$_['error_country']        = 'Выберите страну!';
-$_['error_zone']           = 'Выберите регион / область!';
-$_['error_agree']          = 'Для совершения покупок Вы должны быть согласны с документом %s!';
+$_['error_exists']         = 'Цей E-Mail вже зареєстрований!';
+$_['error_firstname']      = 'Ім’я повинно бути від 1 до 32 символів!';
+$_['error_lastname']       = 'Прізвище повинно бути від 1 до 32 символів!';
+$_['error_email']          = 'E-Mail введено невірно!';
+$_['error_telephone']      = 'Номер телефону повинен бути від 3 до 32 символів!';
+$_['error_password']       = 'Пароль повинен бути від 4 до 20 символів!';
+$_['error_confirm']        = 'Паролі не співпадають!';
+$_['error_company_id']     = 'Вкажіть рег. номер компанії!';
+$_['error_tax_id']         = 'Вкажіть податковий номер!';
+$_['error_vat']            = 'Невірний VAT number!';
+$_['error_address_1']      = 'Адреса повинна бути від 3 до 128 символів!';
+$_['error_city']           = 'Назва міста має бути від 2 до 128 символів!';
+$_['error_postcode']       = 'Індекс повинен бути від 2 до 10 символів!';
+$_['error_country']        = 'Будь ласка, вкажіть країну!';
+$_['error_zone']           = 'Будь ласка, вкажіть регіон / область!';
+$_['error_agree']          = 'Для здійснення покупок Ви повинні бути згодні з документом %s!';
 ?>
