ba16522357362c441d9e68333d0f33c25c4f47a0
ocshop <info@vilkov.info>
2015-04-24 21:59:34 +0300

    catalog/store.php

--- upload/catalog/language/ukrainian/module/store.php
+++ upload/catalog/language/ukrainian/module/store.php
@@ -1,8 +1,13 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title'] = 'Выберите магазин';
+$_['heading_title'] = 'Виберіть магазин';
 
 // Text
-$_['text_default']  = 'Основной';
-$_['text_store']    = 'Пожалуйста, выберите магазин, который хотите посетить.';
+$_['text_default']  = 'Основний';
+$_['text_store']    = 'Будь ласка, виберіть магазин, який хочете відвідати.';
 ?>
