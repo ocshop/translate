4b05429f0c6ec13f8fca53032e8f68311d497cf8
ocshop <info@vilkov.info>
2015-04-24 22:22:07 +0300

    catalog/pp_standard.php

--- upload/catalog/language/ukrainian/payment/pp_standard.php
+++ upload/catalog/language/ukrainian/payment/pp_standard.php
@@ -1,7 +1,12 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
 $_['text_title']    = 'PayPal';
 $_['text_reason'] 	= 'ПРИЧИНА';
-$_['text_testmode']	= 'Платёжный шлюз в режиме \'Песочницы\'. Средства с вашего счёта не будут сниматься.';
-$_['text_total']	= 'Доставка, обработка, скидки и налоги';
+$_['text_testmode']	= 'Платіжний шлюз в режимі \ 'Пісочниці \'. Кошти з вашого рахунку не зніматимуть.';
+$_['text_total']	= 'Доставка, обробка, знижки та податки';
 ?>
