0a9d3e5dc22689de1c310e1a86124af57a97281f
ocshop <info@vilkov.info>
2015-04-25 19:54:28 +0300

    catalog\citylink.php

--- upload/catalog/language/ukrainian/shipping/citylink.php
+++ upload/catalog/language/ukrainian/shipping/citylink.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']  = 'Доставка по городу';
-$_['text_weight'] = 'Вес:';
+$_['text_title']  = 'Доставка по місту';
+$_['text_weight'] = 'Вага:';
 ?>
