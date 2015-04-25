8281c0849cecca33c512511895a9c659d0c0c4e8
ocshop <info@vilkov.info>
2015-04-25 20:12:37 +0300

    catalog\handling.php

--- upload/catalog/language/ukrainian/total/handling.php
+++ upload/catalog/language/ukrainian/total/handling.php
@@ -1,3 +1,8 @@
 <?php
-$_['text_handling'] = 'Оплата обработки заказа:';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+$_['text_handling'] = 'Оплата обробки замовлення:';
 ?>
