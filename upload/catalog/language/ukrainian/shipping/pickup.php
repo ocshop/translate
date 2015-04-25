61c1227744a5435f37fca18931bf87131af4fcda
ocshop <info@vilkov.info>
2015-04-25 20:08:30 +0300

    catalog\pickup.php

--- upload/catalog/language/ukrainian/shipping/pickup.php
+++ upload/catalog/language/ukrainian/shipping/pickup.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']       = 'Самовывоз';
-$_['text_description'] = 'Самовывоз из магазина';
+$_['text_title']       = 'Самовивіз';
+$_['text_description'] = 'Самовивіз з магазину';
 ?>
