54eee46df392b60e05514761d8200ff7d0196d5e
ocshop <info@vilkov.info>
2015-04-25 20:05:26 +0300

    catalog/item.php

--- upload/catalog/language/ukrainian/shipping/item.php
+++ upload/catalog/language/ukrainian/shipping/item.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']       = 'За единицу';
-$_['text_description'] = 'Стоимость доставки за единицу товара';
+$_['text_title']       = 'За одиницю';
+$_['text_description'] = 'Вартість доставки за одиницю товару';
 ?>
