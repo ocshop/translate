192dd033a0fb0ce648f9cd9027c381a80eab61bf
ocshop <info@vilkov.info>
2015-04-25 19:56:15 +0300

    catalog\flat.php

--- upload/catalog/language/ukrainian/shipping/flat.php
+++ upload/catalog/language/ukrainian/shipping/flat.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']       = 'Фиксированная стоимость доставки';
-$_['text_description'] = 'Фиксированная стоимость доставки';
+$_['text_title']       = 'Фіксована вартість доставки';
+$_['text_description'] = 'Фіксована вартість доставки';
 ?>
