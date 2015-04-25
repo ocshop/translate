b750d93faf9face5975b52139c1aad1ea89bafe1
ocshop <info@vilkov.info>
2015-04-25 20:15:59 +0300

    catalog\total.php

--- upload/catalog/language/ukrainian/total/total.php
+++ upload/catalog/language/ukrainian/total/total.php
@@ -1,3 +1,8 @@
 <?php
-$_['text_total'] = 'Итого';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+$_['text_total'] = 'Разом';
 ?>
