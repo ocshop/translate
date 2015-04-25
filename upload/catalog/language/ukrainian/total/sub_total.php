fc03f6ee99c71ef3d6564d89c789f39b014a31d9
ocshop <info@vilkov.info>
2015-04-25 20:15:16 +0300

    catalog\sub_total.php

--- upload/catalog/language/ukrainian/total/sub_total.php
+++ upload/catalog/language/ukrainian/total/sub_total.php
@@ -1,3 +1,8 @@
 <?php
-$_['text_sub_total'] = 'Сумма';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+$_['text_sub_total'] = 'Сума';
 ?>
