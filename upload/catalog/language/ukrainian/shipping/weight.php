4899c7ec7c1dde2f4574250a2390ff095f667a34
ocshop <info@vilkov.info>
2015-04-25 20:09:30 +0300

    catalog\weight.php

--- upload/catalog/language/ukrainian/shipping/weight.php
+++ upload/catalog/language/ukrainian/shipping/weight.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']  = 'Оплата доставки по весу';
-$_['text_weight'] = 'Вес:';
+$_['text_title']  = 'Оплата доставки по вазі';
+$_['text_weight'] = 'Вага:';
 ?>
