7ab62c3138607368b44c8d6cd9671c037921976e
ocshop <info@vilkov.info>
2015-04-25 20:10:26 +0300

    catalog/xshipping.php

--- upload/catalog/language/ukrainian/shipping/xshipping.php
+++ upload/catalog/language/ukrainian/shipping/xshipping.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']       = 'Наши способы доставки';
-$_['text_description'] = 'Наши способы доставки';
+$_['text_title']       = 'Наші способи доставки';
+$_['text_description'] = 'Наші способи доставки';
 ?>
