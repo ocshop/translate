4717d989d87c19b924e2302ac98d44f1ec255abf
ocshop <info@vilkov.info>
2015-04-25 20:13:31 +0300

    catalog/low_order_fee.php

--- upload/catalog/language/ukrainian/total/low_order_fee.php
+++ upload/catalog/language/ukrainian/total/low_order_fee.php
@@ -1,3 +1,8 @@
 <?php
-$_['text_low_order_fee'] = 'Дополнительная оплата при мелких заказах:';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+$_['text_low_order_fee'] = 'Додаткова оплата при дрібних замовленнях:';
 ?>
