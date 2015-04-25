77eb0d120c9a0fb300a2ffccb246782bd3544684
ocshop <info@vilkov.info>
2015-04-25 20:07:38 +0300

    catalog\novaposhta.php

--- upload/catalog/language/ukrainian/shipping/novaposhta.php
+++ upload/catalog/language/ukrainian/shipping/novaposhta.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']       = 'Новая Почта';
-$_['text_description'] = 'Отправим товар Новой Почтой в ваш город.';
+$_['text_title']       = 'Нова Пошта';
+$_['text_description'] = 'Відправимо товар Новою Поштою у ваше місто.';
 ?>
