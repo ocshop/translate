c4e1261893a83cfb95c165702a223cc572626874
ocshop <info@vilkov.info>
2015-04-25 19:45:06 +0300

    catalog\search.php

--- upload/catalog/language/ukrainian/product/search.php
+++ upload/catalog/language/ukrainian/product/search.php
@@ -1,39 +1,44 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']     = 'Поиск';
+$_['heading_title']     = 'Пошук';
 
 // Text
-$_['text_search']       = 'Товары, соответствующие критериям поиска';
-$_['text_keyword']      = 'Ключевые слова';
-$_['text_category']     = 'Все категории';
-$_['text_sub_category'] = 'Поиск в подкатегориях';
-$_['text_critea']       = 'Критерии поиска';
-$_['text_empty']        = 'Нет товаров, которые соответствуют критериям поиска.';
-$_['text_quantity']     = 'Кол-во:';
-$_['text_manufacturer'] = 'Производитель:';
+$_['text_search']       = 'Товари, що відповідають критеріям пошуку';
+$_['text_keyword']      = 'Ключові слова';
+$_['text_category']     = 'Всі категорії';
+$_['text_sub_category'] = 'Пошук в підкатегоріях';
+$_['text_critea']       = 'Критерії пошуку';
+$_['text_empty']        = 'Немає товарів, які відповідають критеріям пошуку.';
+$_['text_quantity']     = 'Кількість:';
+$_['text_manufacturer'] = 'Виробник:';
 $_['text_model']        = 'Модель:';
-$_['text_points']       = 'Бонусные баллы:';
-$_['text_price']        = 'Цена:';
-$_['text_tax']          = 'Без НДС:';
-$_['text_reviews']      = 'На основе %s отзывов.';
-$_['text_compare']      = 'Сравнение товаров (%s)';
+$_['text_points']       = 'Бонусні бали:';
+$_['text_price']        = 'Ціна:'; 
+$_['text_tax']          = 'Без ПДВ:';
+$_['text_reviews']      = 'На основі %s відгуків.';
+$_['text_compare']      = 'Порівняння товарів (%s)'; 
 $_['text_display']      = 'Вид:';
-$_['text_list']         = 'Список';
-$_['text_grid']         = 'Сетка';
-$_['text_sort']         = 'Сортировка:';
-$_['text_default']      = 'По умолчанию';
-$_['text_name_asc']     = 'Название (А - Я)';
-$_['text_name_desc']    = 'Название (Я - А)';
-$_['text_price_asc']    = 'Цена (низкая &gt; высокая)';
-$_['text_price_desc']   = 'Цена (высокая &gt; низкая)';
-$_['text_rating_asc']   = 'Рейтинг (начиная с низкого)';
-$_['text_rating_desc']  = 'Рейтинг (начиная с высокого)';
-$_['text_model_asc']    = 'Модель (А- Я)';
-$_['text_model_desc']   = 'Модель (Я - А)';
-$_['text_limit']        = 'Показать:';
-$_['text_benefits']     = 'Преимущества:';
+$_['text_list']         = 'Списком';
+$_['text_grid']         = 'Таблицею';
+$_['text_sort']         = 'Сортування:';
+$_['text_default']      = 'За замовчуванням';
+$_['text_name_asc']     = 'Найменування (А -&gt; Я)';
+$_['text_name_desc']    = 'Найменування (Я -&gt; А)';
+$_['text_price_asc']    = 'Ціна (за зростанням)';
+$_['text_price_desc']   = 'Ціна (за спаданням)';
+$_['text_rating_asc']   = 'Рейтинг (за зростанням)';
+$_['text_rating_desc']  = 'Рейтинг (за спаданням)';
+$_['text_model_asc']    = 'Модель (А -&gt; Я)';
+$_['text_model_desc']   = 'Модель (Я -&gt; А)';
+$_['text_limit']        = 'Показати:';
+$_['text_benefits']     = 'Переваги:';
 
 // Entry
-$_['entry_search']      = 'Поиск:';
-$_['entry_description'] = 'Искать в описании товаров';
+$_['entry_search']      = 'Пошук:';
+$_['entry_description'] = 'Шукати в описі товарів';
 ?>
