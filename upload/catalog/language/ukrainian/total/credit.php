89b7dac10a129b5bce5174faea1adf37c4293a1c
ocshop <info@vilkov.info>
2015-04-25 20:11:58 +0300

    catalog/credit.php

--- upload/catalog/language/ukrainian/total/credit.php
+++ upload/catalog/language/ukrainian/total/credit.php
@@ -1,4 +1,9 @@
 <?php
-$_['text_credit']   = 'Кредит Магазина:';
-$_['text_order_id'] = 'Номер заказа (ID): #%s';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+$_['text_credit']   = 'Кредит Магазину:';
+$_['text_order_id'] = 'Номер замовлення (ID): #%s';
 ?>
