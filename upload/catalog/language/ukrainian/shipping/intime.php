9189f2450b1b26db6caa7f0e085ef56f2fe9456d
ocshop <info@vilkov.info>
2015-04-25 20:04:30 +0300

    catalog\intime.php

--- upload/catalog/language/ukrainian/shipping/intime.php
+++ upload/catalog/language/ukrainian/shipping/intime.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']       = 'Ин-Тайм';
-$_['text_description'] = 'Отправим товар Ин-Таймом в ваш город.';
+$_['text_title']       = 'Ін-Тайм';
+$_['text_description'] = 'Відправимо товар Ін-Таймом у ваше місто.';
 ?>
