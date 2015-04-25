a8ad693a175f6026799d14162cfe5e86971ffdec
ocshop <info@vilkov.info>
2015-04-25 20:06:28 +0300

    catalog/nexpress.php

--- upload/catalog/language/ukrainian/shipping/nexpress.php
+++ upload/catalog/language/ukrainian/shipping/nexpress.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_title']       = 'Ночной экспресс';
-$_['text_description'] = 'Отправим товар Ночным экспрессом в ваш город.';
+$_['text_title']       = 'Нічний експрес';
+$_['text_description'] = 'Відправимо товар Нічним експресом у ваше місто.';
 ?>
