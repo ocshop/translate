645a5f3d6aa80b398b89a9e3863cbcbc4bd99f1c
ocshop <info@vilkov.info>
2015-04-25 20:14:36 +0300

    catalog\reward.php

--- upload/catalog/language/ukrainian/total/reward.php
+++ upload/catalog/language/ukrainian/total/reward.php
@@ -1,5 +1,10 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_reward']   = 'Бонусные баллы (%s)';
-$_['text_order_id'] = 'Заказ №: %s';
+$_['text_reward']   = 'Бонусні бали (%s)';
+$_['text_order_id'] = 'Замовлення №: %s';
 ?>
