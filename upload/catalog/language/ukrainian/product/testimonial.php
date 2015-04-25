339eac8aa9261fa2f38d8f94f18cb018ac94f9c1
ocshop <info@vilkov.info>
2015-04-25 19:52:34 +0300

    catalog\testimonial.php

--- upload/catalog/language/ukrainian/product/testimonial.php
+++ upload/catalog/language/ukrainian/product/testimonial.php
@@ -1,19 +1,24 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title'] 	= 'Отзывы о Магазине';
+$_['heading_title'] 	= 'Відгуки про Магазин';
 
 $_['text_average']      = 'Рейтинг:';
-$_['text_stars']        = '%s из 5 !';
-$_['text_no_rating']    = 'Без рейтинга';
-$_['text_rating_all']   = 'Все ';
-$_['text_rating_good']  = 'Положительные ';
-$_['text_rating_bad']   = 'Отрицательные ';
+$_['text_stars']        = '%s з 5 !';
+$_['text_no_rating']    = 'Без рейтингу';
+$_['text_rating_all']   = 'Усі ';
+$_['text_rating_good']  = 'Позитивні ';
+$_['text_rating_bad']   = 'Негативні ';
 
 // Text
-$_['text_error'] 		= 'Отзывы: Нет ни одного отзыва!';
-$_['text_pagination'] 	= 'Отзывы с {start} по {end} из {total} (всего страниц - {pages})';
-$_['text_showall'] 	= 'Показать все отзывы';
-$_['text_write'] 		= 'Написать отзыв';
+$_['text_error'] 		= 'Відгуки: Немає жодного відгуку!';
+$_['text_pagination'] 	= 'Відгуки з {start} по {end} з {total} (усього сторінок - {pages})';
+$_['text_showall'] 	= 'Показати всі відгуки';
+$_['text_write'] 		= 'Написати відгук';
 $_['text_auteur'] 	= 'Автор: %s ';
 $_['text_city'] 		= ' ( %s )';
 ?>
