c6c6a19da6ab77e4c17e4793fc1f1fa5239f15dd
ocshop <info@vilkov.info>
2015-05-06 15:34:03 +0300

    admin\news.php

--- upload/admin/language/ukrainian/blog/news.php
+++ upload/admin/language/ukrainian/blog/news.php
@@ -1,37 +1,42 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']          = 'Категории';
+$_['heading_title']          = 'Категорії';
 
 // Text
-$_['text_success']           = 'Список категорий обновлен!';
-$_['text_default']           = 'Основной магазин';
-$_['text_image_manager']     = 'Менеджер изображений';
-$_['text_browse']            = 'Обзор';
-$_['text_clear']             = 'Очистить';
+$_['text_success']           = 'Список категорій оновлено!';
+$_['text_default']           = 'Основний магазин';
+$_['text_image_manager']     = 'Менеджер зображень';
+$_['text_browse']            = 'Огляд';
+$_['text_clear']             = 'Очистити';
 
 // Column
-$_['column_name']            = 'Название категории';
-$_['column_sort_order']      = 'Порядок сортировки';
-$_['column_action']          = 'Действие';
+$_['column_name']            = 'Назва категорії';
+$_['column_sort_order']      = 'Порядок сортування';
+$_['column_action']          = 'Дія';
 
 // Entry
-$_['entry_name']             = 'Название категории:';
+$_['entry_name']             = 'Назва категорії:';
 $_['entry_meta_keyword']     = 'Мета-тег Keywords:';
-$_['entry_meta_description'] = 'Мета-тег Description:';
-$_['entry_description']      = 'Описание:';
-$_['entry_parent']         	 = 'Родительская категория:';
-$_['entry_store']            = 'Магазины:';
-$_['entry_keyword']          = 'SEO URL:<br/><span class="help">Должно быть уникальным на всю систему.</span>';
-$_['entry_image']            = 'Изображение категории:';
-$_['entry_top']              = 'Галерея:<br/><span class="help">Укажите если категория для галерей.</span>';
-$_['entry_sort_order']       = 'Порядок сортировки:';
+$_['entry_meta_description'] = 'Мета-тег "Description:":';
+$_['entry_description']      = 'Опис:';
+$_['entry_parent']         	 = 'Батьківська категорія:';
+$_['entry_store']            = 'Магазини:';
+$_['entry_keyword']          = 'SEO URL:<br/><span class="help">Повинно бути унікальним на всю систему.</span>';
+$_['entry_image']            = 'Зображення категорії:';
+$_['entry_top']              = 'Галерея:<br/><span class="help">Вкажіть якщо категорія для галерей.</span>';
+$_['entry_sort_order']       = 'Порядок сортування:';
 $_['entry_status']           = 'Статус:';
-$_['entry_layout']           = 'Изменить схему:';
+$_['entry_layout']           = 'Змінити схему:';
 $_['entry_seo_title']        = 'HTML-тег Title:';
 $_['entry_seo_h1']           = 'HTML-тег H1:';
 
 // Error
-$_['error_warning']          = 'Внимательно проверьте форму на ошибки!';
-$_['error_permission']       = 'У Вас нет прав для изменения категорий!';
-$_['error_name']             = 'Название категории должно быть от 2 до 32 символов!';
+$_['error_warning']          = 'Уважно перевірте форму на помилки!';
+$_['error_permission']       = 'У Вас немає прав для зміни категорій!';
+$_['error_name']             = 'Назва категорії має бути від 2 до 32 символів!';
 ?>
