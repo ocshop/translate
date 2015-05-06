2ae518e3be92816f8831d9865e987f20c400ad43
ocshop <info@vilkov.info>
2015-05-06 15:51:55 +0300

    admin\attribute.php

--- upload/admin/language/ukrainian/catalog/attribute.php
+++ upload/admin/language/ukrainian/catalog/attribute.php
@@ -1,23 +1,28 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']				= 'Атрибуты';
+$_['heading_title']				= 'Атрибути';
 
 // Text
-$_['text_success']				= 'Атрибуты успешно обновлены!';
+$_['text_success']				= 'Атрибути успішно оновлені!';
 
 // Column
-$_['column_name']				= 'Название атрибута';
-$_['column_attribute_group']	= 'Группа атрибута';
-$_['column_sort_order']			= 'Порядок сортировки';
-$_['column_action']				= 'Действие';
+$_['column_name']				= 'Назва атрибуту';
+$_['column_attribute_group']	= 'Група атрибуту';
+$_['column_sort_order']			= 'Порядок сортування';
+$_['column_action']				= 'Дія';
 
 // Entry
-$_['entry_name']				= 'Название атрибута:';
-$_['entry_attribute_group']		= 'Группа атрибута:';
-$_['entry_sort_order']			= 'Порядок сортировки:';
+$_['entry_name']				= 'Назва атрибуту:';
+$_['entry_attribute_group']		= 'Група атрибуту:';
+$_['entry_sort_order']			= 'Порядок сортування:';
 
 // Error
-$_['error_permission']			= 'У Вас нет прав для изменения атрибутов!';
-$_['error_name']				= 'Название атрибута должно быть от 3 до 64 символов!';
-$_['error_product']				= 'Этот атрибут не может быть удален, так как назначен %s товарам!';
+$_['error_permission']			= 'У Вас немає прав для зміни атрибутів!';
+$_['error_name']				= 'Назва атрибуту повинна бути від 3 до 64 символів!';
+$_['error_product']				= 'Цей атрибут не може бути видалений, так як призначений %s товарам!';
 ?>
