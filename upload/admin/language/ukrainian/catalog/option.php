42757434064f8dcf9b904b198bed448997853fe2
ocshop <info@vilkov.info>
2015-05-06 19:11:32 +0300

    admin/option.php

--- upload/admin/language/ukrainian/catalog/option.php
+++ upload/admin/language/ukrainian/catalog/option.php
@@ -1,41 +1,46 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']       = 'Опции';
+$_['heading_title']       = 'Опції';
 
 // Text
-$_['text_success']        = 'Опции изменены!';
-$_['text_choose']         = 'Выбор';
-$_['text_select']         = 'Список';
-$_['text_radio']          = 'Переключатель';
-$_['text_checkbox']       = 'Флажок';
-$_['text_image']          = 'Изображение';
-$_['text_input']          = 'Поле ввода';
+$_['text_success']        = 'Опції змінені!';
+$_['text_choose']         = 'Вибір';
+$_['text_select']         = 'Випадаючий список';
+$_['text_radio']          = 'Перемикач';
+$_['text_checkbox']       = 'Чекбокс';
+$_['text_image']          = 'Зображення';
+$_['text_input']          = 'Поле введення';
 $_['text_text']           = 'Текст';
-$_['text_textarea']       = 'Текстовая область';
+$_['text_textarea']       = 'Текстова область';
 $_['text_file']           = 'Файл';
 $_['text_date']           = 'Дата';
-$_['text_datetime']       = 'Дата и время';
-$_['text_time']           = 'Время';
-$_['text_image_manager']  = 'Управление изображениями';
-$_['text_browse']         = 'Просмотр файлов';
-$_['text_clear']          = 'Убрать изображение';
+$_['text_datetime']       = 'Дата і час';
+$_['text_time']           = 'Час';
+$_['text_image_manager']  = 'Управління зображеннями';
+$_['text_browse']         = 'Перегляд файлів';
+$_['text_clear']          = 'Видалити зображення';
 
 // Column
-$_['column_name']         = 'Опция';
-$_['column_sort_order']   = 'Порядок сортировки';
-$_['column_action']       = 'Действие';
+$_['column_name']         = 'Опція';
+$_['column_sort_order']   = 'Порядок сортування';
+$_['column_action']       = 'Дія';
 
 // Entry
-$_['entry_name']         = 'Название опции:';
+$_['entry_name']         = 'Назва опції:';
 $_['entry_type']         = 'Тип:';
-$_['entry_option_value'] = 'Значение опции:';
-$_['entry_image']        = 'Image:';
-$_['entry_sort_order']   = 'Порядок сортировки:';
+$_['entry_option_value'] = 'Значення опції:';
+$_['entry_image']        = 'Зображення:';
+$_['entry_sort_order']   = 'Порядок сортування:';
 
 // Error
-$_['error_permission']   = 'У вас не достаточно прав для изменения опции!';
-$_['error_name']         = 'Имя опции должно быть от 1 до 128 символов!';
-$_['error_type']         = 'Не указано значение опции!';
-$_['error_option_value'] = 'Значение опции должно быть от 1 до 128!';
-$_['error_product']      = 'Эта опция не может быть удалена, поскольку она в настоящий момент присвоена %s товарам!';
+$_['error_permission']   = 'У вас не достатньо прав для зміни опції!';
+$_['error_name']         = 'Ім’я опції повинно бути від 1 до 128 символів!';
+$_['error_type']         = 'Не вказано значення опції!';
+$_['error_option_value'] = 'Значення опції повинно бути від 1 до 128!';
+$_['error_product']      = 'Ця опція не може бути вилучена, оскільки вона зараз присвоєна %s товарам!';
 ?>
