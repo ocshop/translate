984c3b801a129e1410bc078679619439c9d79236
ocshop <info@vilkov.info>
2015-05-06 17:22:04 +0300

    admin\coolfilter.php

--- upload/admin/language/ukrainian/catalog/coolfilter.php
+++ upload/admin/language/ukrainian/catalog/coolfilter.php
@@ -1,50 +1,55 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']          = 'Фильтр товаров';
+$_['heading_title']          = 'Фільтр товарів';
 
 // Text
-$_['text_success']           = 'Список опций фильтра обновлен!';
-$_['text_default']           = 'Основной магазин';
-$_['text_delete']            = 'Удалить';
-$_['text_value']             = 'Значение';
-$_['text_add_value']         = 'Добавить значение';
+$_['text_success']           = 'Список опцій фільтра оновлено!';
+$_['text_default']           = 'Основний магазин';
+$_['text_delete']            = 'Видалити';
+$_['text_value']             = 'Значення';
+$_['text_add_value']         = 'Додати значення';
 
 // Column
-$_['column_name']            = 'Название опции';
-$_['column_values']          = 'Значения';
-$_['column_style']           = 'Стиль фильтра';
-$_['column_type']            = 'Тип фильтра';
-$_['column_coolfilter_groups']	 = 'Группы фильтров';
-$_['column_sort_order']      = 'Порядок сортировки';
+$_['column_name']            = 'Назва опції';
+$_['column_values']          = 'Значення';
+$_['column_style']           = 'Стиль фільтра';
+$_['column_type']            = 'Тип фільтра';
+$_['column_coolfilter_groups']	 = 'Групи фільтрів';
+$_['column_sort_order']      = 'Порядок сортування';
 $_['column_status']          = 'Статус';
-$_['column_action']          = 'Действие';
+$_['column_action']          = 'Дія';
 
 // Entry
-$_['entry_name']             = 'Название опции:';
-$_['entry_values']           = 'Значения опции:';
-$_['entry_category']         = 'Категории:';
+$_['entry_name']             = 'Назва опції:';
+$_['entry_values']           = 'Значення опції:';
+$_['entry_category']         = 'Категорії:';
 $_['entry_status']           = 'Статус:';
-$_['entry_store']            = 'Магазины:';
-$_['entry_sort_order']       = 'Порядок сортировки:';
-$_['entry_type']			 = 'Тип фильтра:';
-$_['entry_style']			 = 'Стиль фильтра:';
-$_['entry_coolfilter_groups']    = 'Группы фильтров:';
+$_['entry_store']            = 'Магазини:';
+$_['entry_sort_order']       = 'Порядок сортування:';
+$_['entry_type']			 = 'Тип фільтра:';
+$_['entry_style']			 = 'Стиль фільтра:';
+$_['entry_coolfilter_groups']    = 'Групи фільтрів:';
 
 // TypeItems
-$_['text_prices']       	 = 'Цена';
-$_['text_manufacters']       = 'Производители';
-$_['text_options']           = 'Опции';
-$_['text_attributes']        = 'Атрибуты';
-$_['text_parameteres']       = 'Параметры';
+$_['text_prices']       	 = 'Ціна';
+$_['text_manufacters']       = 'Виробники';
+$_['text_options']           = 'Опції';
+$_['text_attributes']        = 'Атрибути';
+$_['text_parameteres']       = 'Параметри';
 
 // StyleItems
 $_['style_slider']           = 'Бегунок';
 $_['style_list']             = 'Список';
-$_['style_checkbox']         = 'Чекбоксы';
-$_['style_selected']         = 'Выпадающий список';
-$_['style_images']           = 'Изображения';
+$_['style_checkbox']         = 'Чекбокси';
+$_['style_selected']         = 'Випадаючий список';
+$_['style_images']           = 'Зображення';
 
 // Error
-$_['error_permission']       = 'У Вас нет прав для изменения категорий!';
-$_['error_required_data']    = 'Не введены необходимые данные!';
+$_['error_permission']       = 'У Вас немає прав для зміни фільтру!';
+$_['error_required_data']    = 'Не введені необхідні дані!';
 ?>
