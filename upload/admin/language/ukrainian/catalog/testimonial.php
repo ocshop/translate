ea351af85d9f115133442d044d72957ac1363bff
ocshop <info@vilkov.info>
2015-05-06 22:03:56 +0300

    admin/testimonial.php

--- upload/admin/language/ukrainian/catalog/testimonial.php
+++ upload/admin/language/ukrainian/catalog/testimonial.php
@@ -1,37 +1,42 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']     	= 'Отзывы о Магазине';
+$_['heading_title']     	= 'Відгуки про Магазин';
 
 // Text
-$_['text_success']      	= 'Список отзывов обновлен!';
-$_['text_enabled']      	= 'Опубликован';
-$_['text_disabled']     	= 'Скрыт';
-$_['text_module_settings']   	= 'Настройки модуля';
+$_['text_success']      	= 'Список відгуків оновлено!';
+$_['text_enabled']      	= 'Опублікований';
+$_['text_disabled']     	= 'Прихован';
+$_['text_module_settings']   	= 'Налаштування модуля';
 
 // Column
 $_['column_title']      	= 'Тема';
-$_['column_name']			= 'Имя';
-$_['column_city']			= 'Город';
-$_['column_status']		= 'Статус';
-$_['column_action']     	= 'Действие';
+$_['column_name']			= 'Ім’я';
+$_['column_city']			= 'Місто';
+$_['column_status']			= 'Статус';
+$_['column_action']     	= 'Дія';
 $_['column_date_added'] 	= 'Дата';
-$_['column_description'] 	= 'Сообщение';
+$_['column_description'] 	= 'Повідомлення';
 
 // Entry
 $_['entry_title']       	= 'Тема:';
 $_['entry_status']      	= 'Статус:';
-$_['entry_description'] 	= 'Сообщение:';
+$_['entry_description'] 	= 'Повідомлення:';
 $_['entry_date_added']  	= 'Дата:';
-$_['entry_name']        	= 'Имя:';
-$_['entry_city']			= 'Город:';
+$_['entry_name']        	= 'Ім’я:';
+$_['entry_city']			= 'Місто:';
 $_['entry_rating']        	= 'Рейтинг:';
-$_['entry_good']          	= 'Отлично';
-$_['entry_bad']           	= 'Плохо';
-$_['entry_install_first']  	= 'Пожалуйста, установите сначала модуль Отзывы о Магазине!';
+$_['entry_good']          	= 'Добре';
+$_['entry_bad']           	= 'Погано';
+$_['entry_install_first']  	= 'Будь ласка, встановіть спочатку модуль Відгуки про Магазин!';
 $_['entry_email']        	= 'E-Mail:';
 
 // Error 
-$_['error_permission']  	= 'У Вас нет прав для изменения отзывов!';
-$_['error_name']       		= 'Поле \'Автор\' не должно быть пустым!';
-$_['error_description'] 	= 'Поле \'Сообщение\' не должно быть пустым!';
+$_['error_permission']  = 'У Вас немає прав для зміни відгуків!';
+$_['error_name']       		= 'Ім’я автора повинно бути від 3 до 64 символів!';
+$_['error_description'] 	= 'Текст відгуку повинен містити хоча б 1 символ!';
 ?>
