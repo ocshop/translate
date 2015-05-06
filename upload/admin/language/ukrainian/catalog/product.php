a49d2f4c77bc3a5566cc625ab6047277db210ba8
ocshop <info@vilkov.info>
2015-05-06 20:44:38 +0300

    admin\product.php

--- upload/admin/language/ukrainian/catalog/product.php
+++ upload/admin/language/ukrainian/catalog/product.php
@@ -1,97 +1,102 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']          = 'Товары';
+$_['heading_title']          = 'Товари';
 
 // Text
-$_['text_success']           = 'Список товаров обновлен!';
+$_['text_success']           = 'Список товарів оновлено!';
 $_['text_plus']              = '+';
 $_['text_minus']             = '-';
-$_['text_default']           = 'Основной магазин';
-$_['text_image_manager']     = 'Управление изображениями';
-$_['text_browse']            = 'Просмотр файлов';
-$_['text_clear']             = 'Убрать изображение';
-$_['text_option']            = 'Опция';
-$_['text_option_value']      = 'Значение опции';
-$_['text_percent']           = 'Процент';
-$_['text_amount']            = 'Фиксированная сумма';
-$_['text_new_price']            = 'Цена успешно изменена. Новая цена: ';
-$_['text_new_quantity']         = 'Количество успешно изменено. Новое количество: ';
-$_['text_new_model']            = 'Модель успешно изменена. Новое значение: ';
-$_['text_new_name']             = 'Название успешно изменено. Новое значение: ';
+$_['text_default']           = 'Основний магазин';
+$_['text_image_manager']     = 'Управління зображеннями';
+$_['text_browse']            = 'Перегляд файлів';
+$_['text_clear']             = 'Видалити зображення';
+$_['text_option']            = 'Опція';
+$_['text_option_value']      = 'Значення опції';
+$_['text_percent']           = 'Відсоток';
+$_['text_amount']            = 'Фіксована сума';
+$_['text_new_price']            = 'Ціна успішно змінена. Нова ціна: ';
+$_['text_new_quantity']         = 'Кількість успішно змінено. Нова кількість: ';
+$_['text_new_model']            = 'Модель успішно змінена. Нове значення: ';
+$_['text_new_name']             = 'Модель успішно змінена. Нове значення: ';
 
 // Column
-$_['column_name']            = 'Название товара';
-$_['column_category']        = 'Категория';
-$_['column_manufacturer']    = 'Производитель';
+$_['column_name']            = 'Назва товару';
+$_['column_category']        = 'Категорія';
+$_['column_manufacturer']    = 'Виробник';
 $_['column_model']           = 'Модель';
-$_['column_image']           = 'Изображения';
+$_['column_image']           = 'Зображення';
 
-$_['button_clear']           = 'Очистить';
+$_['button_clear']           = 'Очистити';
 
 // End add
-$_['column_price']           = 'Цена на сайте';
-$_['column_quantity']        = 'Количество';
+$_['column_price']           = 'Ціна на сайті';
+$_['column_quantity']        = 'Кількість';
 $_['column_status']          = 'Статус';
-$_['column_action']          = 'Действие';
+$_['column_action']          = 'Дія';
 $_['tab_marketplace_links']  = 'Marketplace';
 
 // Entry
-$_['entry_name']             = 'Название товара:';
-$_['entry_meta_keyword'] 	 = 'META Keywords:<br/><span class="help">Мета-тег "Ключевые слова"</span>';
-$_['entry_meta_description'] = 'META Description:<br/><span class="help">Мета-тег "Описание"</span>';
-$_['entry_description']      = 'Описание:';
-$_['entry_description_mini'] = 'Краткое Описание:';
-$_['entry_store']            = 'Магазины:';
-$_['entry_keyword']          = 'SEO URL:<br/><span class="help">Должно быть уникальным на всю систему.</span>';
+$_['entry_name']             = 'Назва товару:';
+$_['entry_meta_keyword'] 	 = 'META Keywords:<br/><span class="help">Мета-тег "Ключові слова"</span>';
+$_['entry_meta_description'] = 'META Description:<br/><span class="help">Мета-тег "Опис"</span>';
+$_['entry_description']      = 'Опис:';
+$_['entry_description_mini'] = 'Короткий Опис:';
+$_['entry_store']            = 'Магазини:';
+$_['entry_keyword']          = 'SEO Keyword:<br /><span class="help">Замінюйте пробіли на знак "-". Значення цього поля має бути унікальним на всю систему.</span>';
 $_['entry_model']            = 'Модель:';
-$_['entry_sku']              = 'Артикул (SKU, код производителя):<br/><span class="help">SKU: Stock Keeping Unit, внутренний складской уникальный идентификатор товара</span>';
-$_['entry_upc']              = 'UPC:<br/><span class="help">Universal Product Code, универсальный общепризнанный уникальный код этого товара</span>';
+$_['entry_sku']              = 'Артикул (SKU, код производителя):<br/><span class="help">SKU: Stock Keeping Unit, внутрішній складський унікальний ідентифікатор товару</span>';
+$_['entry_upc']              = 'UPC:<br/><span class="help">Universal Product Code, універсальний загальновизнаний унікальний код цього товару</span>';
 $_['entry_ean']              = 'EAN:<br/><span class="help">European Article Number</span>';
 $_['entry_jan']              = 'JAN:<br/><span class="help">Japanese Article Number</span>';
 $_['entry_isbn']             = 'ISBN:<br/><span class="help">International Standard Book Number</span>';
 $_['entry_mpn']              = 'MPN:<br/><span class="help">Manufacturer Part Number</span>';
-$_['entry_location']         = 'Расположение:';
-$_['entry_shipping']         = 'Необходима доставка:';
-$_['entry_manufacturer']     = 'Производитель:';
-$_['entry_date_available']   = 'Дата поступления:';
-$_['entry_quantity']         = 'Количество:';
-$_['entry_minimum']          = 'Минимальное количество:<br/><span class="help">Указание минимально количества в заказе</span>';
-$_['entry_stock_status']     = 'Статус при отсутствии на складе:<br /><span class="help">Статус, показываемый, когда товара нет на складе</span>';
-$_['entry_price']            = 'Цена:';
-$_['entry_tax_class']        = 'Налог:';
-$_['entry_points']           = 'Баллы:<br/><span class="help">Количество баллов для покупки товара. Поставьте 0, чтобы не использовать баллы.</span>';
-$_['entry_option_points']    = 'Баллы:';
-$_['entry_subtract']         = 'Вычитать со склада:';
-$_['entry_weight_class']     = 'Единица измерения веса:';
-$_['entry_weight']           = 'Вес:';
-$_['entry_length']           = 'Единица измерения длины:';
-$_['entry_dimension']        = 'Размеры (длина x ширина x высота):';
-$_['entry_image']            = 'Изображение товара:';
-$_['entry_customer_group']   = 'Группа покупателей:';
-$_['entry_date_start']       = 'Дата начала:';
-$_['entry_date_end']         = 'Дата окончания:';
-$_['entry_priority']         = 'Приоритет:';
+$_['entry_location']         = 'Розташування:';
+$_['entry_shipping']         = 'Необхідна доставка:';
+$_['entry_manufacturer']     = 'Виробник:';
+$_['entry_date_available']   = 'Дата надходження:';
+$_['entry_quantity']         = 'Кількість:';
+$_['entry_minimum']          = 'Мінімальна кількість:<br/><span class="help">Зазначення мінімальної кількості в замовленні</span>';
+$_['entry_stock_status']     = 'Статус при відсутності на складі:<br /><span class="help">Статус, що показується, коли товару немає на складі</span>';
+$_['entry_price']            = 'Ціна:';
+$_['entry_tax_class']        = 'Податок:';
+$_['entry_points']           = 'Бали:<br/><span class="help">Кількість балів для купівлі товару. Поставте 0, щоб не використовувати бали.</span>';
+$_['entry_option_points']    = 'Бали:';
+$_['entry_subtract']         = 'Віднімати зі складу:';
+$_['entry_weight_class']     = 'Одиниця виміру ваги:';
+$_['entry_weight']           = 'Вага:';
+$_['entry_length']           = 'Одиниця виміру довжини:';
+$_['entry_dimension']        = 'Розміри (довжина x ширина x висота):';
+$_['entry_image']            = 'Зображення товару:';
+$_['entry_customer_group']   = 'Група покупців:';
+$_['entry_date_start']       = 'Дата початку:';
+$_['entry_date_end']         = 'Дата завершення:';
+$_['entry_priority']         = 'Пріоритет:';
 $_['entry_attribute']        = 'Атрибут:';
-$_['entry_attribute_group']  = 'Группы атрибутов:';
+$_['entry_attribute_group']  = 'Групи атрибутів:';
 $_['entry_text']             = 'Текст:';
-$_['entry_option']           = 'Опция:';
-$_['entry_option_value']     = 'Значение опции:';
-$_['entry_required']         = 'Необходимо:';
+$_['entry_option']           = 'Опція:';
+$_['entry_option_value']     = 'Значення опції:';
+$_['entry_required']         = 'Необхідно:';
 $_['entry_status']           = 'Статус:';
-$_['entry_sort_order']       = 'Порядок сортировки:';
-$_['entry_category']         = 'Показывать в категориях:<br /><span class="help">(автодополнение)</span>';
-$_['entry_filter']           = 'Фильтры:<br /><span class="help">(автодополнение)</span>';
-$_['entry_download']         = 'Загрузки:';
-$_['entry_related']          = 'Похожие товары:<br/><span class="help">(Автодополнение)</span>';
-$_['entry_related2']         = 'Рекомендуемые:<br/><span class="help">(Автодополнение)</span>';
-$_['entry_blog_related']          = 'Рекомендуем почитать:<br/><span class="help">(Автодополнение)</span>';
-$_['entry_tag']          	 = 'Теги товара:<br /><span class="help">разделяются запятой</span>';
-$_['entry_reward']           = 'Бонусные баллы:';
-$_['entry_layout']           = 'Изменить схему:';
-$_['entry_main_category']    = 'Главная категория:';
+$_['entry_sort_order']       = 'Порядок сортування:';
+$_['entry_category']         = 'Показувати в категоріях:<br /><span class="help">(Автодоповнення)</span>';
+$_['entry_filter']           = 'Фільтри:<br /><span class="help">(Автодоповнення)</span>';
+$_['entry_download']         = 'Завантаження:';
+$_['entry_related']          = 'Схожі товари:<br/><span class="help">(Автодоповнення)</span>';
+$_['entry_related2']         = 'Рекомендовані:<br/><span class="help">(Автодоповнення)</span>';
+$_['entry_blog_related']     = 'Рекомендуємо почитати:<br/><span class="help">(Автодоповнення)</span>';
+$_['entry_tag']          	 = 'Теги товару:<br /><span class="help">разделяются запятой</span>';
+$_['entry_reward']           = 'Бонусні бали:';
+$_['entry_layout']           = 'Змінити схему:';
+$_['entry_main_category']    = 'Головна категорія:';
 $_['entry_seo_title']        = 'HTML-тег Title:';
 $_['entry_seo_h1']           = 'HTML-тег H1:';
-$_['entry_profile']          = 'Профиль:';
+$_['entry_profile']          = 'Профіль:';
 $_['text_recurring_help']    = 'Recurring amounts are calculated by the frequency and cycles. <br />For example if you use a frequency of "week" and a cycle of "2", then the user will be billed every 2 weeks. <br />The length is the number of times the user will make a payment, set this to 0 if you want payments until they are cancelled.';
 $_['text_recurring_title']   = 'Recurring payments';
 $_['text_recurring_trial']   = 'Trial period';
@@ -107,22 +112,22 @@ $_['entry_trial_cycle']      = 'Trial recurring cycles:<span class="help">How of
 $_['entry_trial_length']     = 'Trial recurring length:';
 
 $_['text_length_day']        = 'День';
-$_['text_length_week']       = 'Неделя';
-$_['text_length_month']      = 'Месяц';
-$_['text_length_month_semi'] = 'Пол месяца';
-$_['text_length_year']       = 'Год';
+$_['text_length_week']       = 'Тиждень';
+$_['text_length_month']      = 'Місяць';
+$_['text_length_month_semi'] = 'Пол місяця';
+$_['text_length_year']       = 'Рік';
 
 //stickers
-$_['text_corner0']           = 'Верхний левый';
-$_['text_corner1']           = 'Верхний правый';
-$_['text_corner2']           = 'Нижний левый';
-$_['text_corner3']           = 'Нижний правый';
-$_['entry_sticker']          = 'Промо Стикеры:';
-$_['text_benefits']         = 'Преимущества:';
+$_['text_corner0']           = 'Верхній лівий';
+$_['text_corner1']           = 'Верхній правий';
+$_['text_corner2']           = 'Нижній лівий';
+$_['text_corner3']           = 'Нижній правий';
+$_['entry_sticker']          = 'Промо Стікери:';
+$_['text_benefits']          = 'Переваги:';
 
 // Error
-$_['error_warning']          = 'Внимательно проверьте форму на ошибки!';
-$_['error_permission']       = 'У Вас нет прав для изменения товаров!';
-$_['error_name']             = 'Название товара должно быть от 3 до 255 символов!';
-$_['error_model']            = 'Модель товара должна быть от 3 до 64 символов!';
+$_['error_warning']          = 'Уважно перевірте форму на помилки!';
+$_['error_permission']       = 'У Вас немає прав для зміни товарів!';
+$_['error_name']             = 'Назва товару має бути від 3 до 255 символів!';
+$_['error_model']            = 'Модель товару повинна бути від 3 до 64 символів!';
 ?>
