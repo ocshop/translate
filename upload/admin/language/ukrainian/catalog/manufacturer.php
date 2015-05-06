8949b3fefa0b217f1456274a356629e5cc884a28
ocshop <info@vilkov.info>
2015-05-06 18:04:25 +0300

    admin\manufacturer.php

--- upload/admin/language/ukrainian/catalog/manufacturer.php
+++ upload/admin/language/ukrainian/catalog/manufacturer.php
@@ -1,40 +1,45 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']      = 'Производители';
+$_['heading_title']      = 'Виробники';
 
 // Text
-$_['text_success']       = 'Список производителей обновлен!';
-$_['text_default']       = 'Основной магазин';
-$_['text_image_manager'] = 'Управление изображениями';
-$_['text_browse']        = 'Просмотр файлов';
-$_['text_clear']         = 'Убрать изображение';
-$_['text_percent']       = 'Процент';
-$_['text_amount']        = 'Фиксированная сумма';
+$_['text_success']       = 'Список виробників оновлено!';
+$_['text_default']       = 'Основний магазин';
+$_['text_image_manager'] = 'Управління зображеннями';
+$_['text_browse']        = 'Перегляд файлів';
+$_['text_clear']         = 'Видалити зображення';
+$_['text_percent']       = 'Відсоток';
+$_['text_amount']        = 'Фіксована сума';
 
 // Column
-$_['column_name']        = 'Производитель';
-$_['column_sort_order']  = 'Порядок сортировки';
-$_['column_action']      = 'Действие';
+$_['column_name']        = 'Виробник';
+$_['column_sort_order']  = 'Порядок сортування';
+$_['column_action']      = 'Дія';
 
 // Entry
-$_['entry_name']         = 'Производитель:';
-$_['entry_store']        = 'Магазины:';
-$_['entry_keyword']      = 'SEO URL:<br/><span class="help">Должно быть уникальным на всю систему.</span>';
-$_['entry_description']  = 'Описание Верх:';
-$_['entry_description_bottom']  = 'Описание Низ:';
-$_['entry_image']        = 'Изображение:';
-$_['entry_sort_order']   = 'Порядок сортировки:';
-$_['entry_related_mn']          = 'Рекомендуемые товары:';
-$_['entry_related_article']     = 'Рекомендуем почитать:';
-$_['entry_layout']       = 'Изменить схему:';
+$_['entry_name']         = 'Виробник:';
+$_['entry_store']        = 'Магазини:';
+$_['entry_keyword']      = 'SEO Keyword:<br /><span class="help">Замінюйте пробіли на знак "-". Значення цього поля має бути унікальним на всю систему.</span>';
+$_['entry_description']  = 'Опис Верх:';
+$_['entry_description_bottom']  = 'Опис Низ:';
+$_['entry_image']        = 'Зображення:';
+$_['entry_sort_order']   = 'Порядок сортування:';
+$_['entry_related_mn']          = 'Рекомендовані товари:';
+$_['entry_related_article']     = 'Рекомендуємо почитати:';
+$_['entry_layout']       = 'Змінити схему:';
 $_['entry_type']         = 'Тип:';
 $_['entry_seo_title']    = 'HTML-тег Title:';
 $_['entry_seo_h1']       = 'HTML-тег H1:';
 $_['entry_meta_keyword'] 		= 'Мета-тег Keywords:';
-$_['entry_meta_description'] = 'META Description:<br/><span class="help">Мета-тег "Описание"</span>';
+$_['entry_meta_description'] = 'META Description:<br/><span class="help">Мета-тег "Опис"</span>';
 
 // Error
-$_['error_permission']   = 'У Вас нет прав для изменения производителей!';
-$_['error_name']         = 'Название производителя должно быть от 3 до 64 символов!';
-$_['error_product']      = 'Этот производитель не может быть удален, поскольку в настоящее время он прикреплен к %s товару(ам)!';
+$_['error_permission']   = 'У Вас немає прав для зміни виробників!';
+$_['error_name']         = 'Назва виробника має бути від 3 до 64 символів!';
+$_['error_product']      = 'Цей виробник не може бути вилучений, оскільки в даний час він прикріплений до %s товару(ам)!';
 ?>
