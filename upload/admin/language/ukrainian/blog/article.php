ca2568676bc6fc9fab8b7a1919f36a0599dc96f3
ocshop <info@vilkov.info>
2015-05-06 14:47:23 +0300

    admin\article.php

--- upload/admin/language/ukrainian/blog/article.php
+++ upload/admin/language/ukrainian/blog/article.php
@@ -1,44 +1,49 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']          = 'Статьи Блога';
+$_['heading_title']          = 'Статті Блогу';
 
 // Text
-$_['text_success']           = 'Список статей обновлен!';
+$_['text_success']           = 'Список статей оновлено!';
 $_['text_plus']              = '+';
 $_['text_minus']             = '-';
-$_['text_default']           = 'Основной магазин';
-$_['text_image_manager']     = 'Менеджер изображений';
-$_['text_browse']            = 'Обзор';
-$_['text_clear']             = 'Очистить';
+$_['text_default']           = 'Основний магазин';
+$_['text_image_manager']     = 'Менеджер зображень';
+$_['text_browse']            = 'Огляд';
+$_['text_clear']             = 'Очистити';
 
 // Column
-$_['column_name']            = 'Название статьи';
-$_['column_image']           = 'Изображения';
+$_['column_name']            = 'Назва статті';
+$_['column_image']           = 'Зображення';
 $_['column_status']          = 'Статус';
-$_['column_action']          = 'Действие';
+$_['column_action']          = 'Дія';
 
 // Entry
-$_['entry_name']             = 'Название статьи:';
+$_['entry_name']             = 'Назва статті:';
 $_['entry_meta_keyword']     = 'Мета-тег Keywords:';
 $_['entry_meta_description'] = 'Мета-тег "Description:":';
-$_['entry_description']      = 'Текст Статьи:';
-$_['entry_store']            = 'Магазины:';
-$_['entry_keyword']          = 'SEO URL:<br/><span class="help">Должно быть уникальным на всю систему.</span>';
-$_['entry_image']            = 'Изображение статьи:';
-$_['entry_status_review']    = 'Статус Комментариев:';
+$_['entry_description']      = 'Текст Статті:';
+$_['entry_store']            = 'Магазини:';
+$_['entry_keyword']          = 'SEO URL:<br/><span class="help">Повинно бути унікальним на всю систему.</span>';
+$_['entry_image']            = 'Зображення статті:';
+$_['entry_status_review']    = 'Статус Коментарів:';
 $_['entry_status']           = 'Статус:';
-$_['entry_sort_order']       = 'Порядок сортировки:';
-$_['entry_category']         = 'Показывать в категориях:';
-$_['entry_download']         = 'Загрузки:';
-$_['entry_related']          = 'Похожие статьи:';
-$_['entry_related_product']  = 'Сопутствующие Товары:';
-$_['entry_layout']           = 'Изменить схему:';
-$_['entry_main_category']    = 'Основная Категория:';
+$_['entry_sort_order']       = 'Порядок сортування:';
+$_['entry_category']         = 'Показувати в категоріях:';
+$_['entry_download']         = 'Завантаження:';
+$_['entry_related']          = 'Схожі статті:';
+$_['entry_related_product']  = 'Супутні Товари:';
+$_['entry_layout']           = 'Змінити схему:';
+$_['entry_main_category']    = 'Основна Категорія:';
 $_['entry_seo_title']        = 'HTML-тег Title:';
 $_['entry_seo_h1']           = 'HTML-тег H1:';
 
 // Error
-$_['error_warning']          = 'Внимательно проверьте форму на ошибки!';
-$_['error_permission']       = 'У Вас нет прав для изменения статей!';
-$_['error_name']             = 'Название статьи должно быть от 3 до 255 символов!';
+$_['error_warning']          = 'Уважно перевірте форму на помилки!';
+$_['error_permission']       = 'У Вас немає прав для зміни статей!';
+$_['error_name']             = 'Назва статті повинна бути від 3 до 255 символів!';
 ?>
