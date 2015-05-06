405b9e0f8b1e2df5b99c9781d405634d06d65ce3
ocshop <info@vilkov.info>
2015-05-06 15:20:07 +0300

    admin\gallery.php

--- upload/admin/language/ukrainian/blog/gallery.php
+++ upload/admin/language/ukrainian/blog/gallery.php
@@ -1,45 +1,50 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']          = 'Галереи';
+$_['heading_title']          = 'Галереї';
 
 // Text
-$_['text_success']           = 'Список галерей обновлен!';
+$_['text_success']           = 'Список галерей оновлено!';
 $_['text_plus']              = '+';
 $_['text_minus']             = '-';
-$_['text_default']           = 'Основной магазин';
-$_['text_image_manager']     = 'Менеджер изображений';
-$_['text_browse']            = 'Обзор';
-$_['text_clear']             = 'Очистить';
+$_['text_default']           = 'Основний магазин';
+$_['text_image_manager']     = 'Менеджер зображень';
+$_['text_browse']            = 'Огляд';
+$_['text_clear']             = 'Очистити';
 
 // Column
-$_['column_name']            = 'Название галереи';
-$_['column_image']           = 'Изображения';
+$_['column_name']            = 'Назва галереї';
+$_['column_image']           = 'Зображення';
 $_['column_status']          = 'Статус';
-$_['column_action']          = 'Действие';
+$_['column_action']          = 'Дія';
 
 // Entry
-$_['entry_name']             = 'Название галереи:';
+$_['entry_name']             = 'Назва галереї:';
 $_['entry_meta_keyword']     = 'Мета-тег Keywords:';
 $_['entry_meta_description'] = 'Мета-тег "Description:":';
-$_['entry_description']      = 'Описание Галереи:';
-$_['entry_store']            = 'Магазины:';
-$_['entry_keyword']          = 'SEO URL:<br/><span class="help">Должно быть уникальным на всю систему.</span>';
-$_['entry_image']            = 'Изображение галереи:';
-$_['entry_status_review']    = 'Статус Комментариев:';
+$_['entry_description']      = 'Опис Галереї:';
+$_['entry_store']            = 'Магазини:';
+$_['entry_keyword']          = 'SEO URL:<br/><span class="help">Повинно бути унікальним на всю систему.</span>';
+$_['entry_image']            = 'Зображення галереї:';
+$_['entry_status_review']    = 'Статус Коментарів:';
 $_['entry_status']           = 'Статус:';
-$_['entry_sort_order']       = 'Порядок сортировки:';
-$_['entry_category']         = 'Показывать в категориях:';
-$_['entry_download']         = 'Загрузки:';
-$_['entry_related']          = 'Похожие галереи:';
-$_['entry_related_article']  = 'Похожие галереи:';
-$_['entry_related_product']  = 'Товары по теме:';
-$_['entry_layout']           = 'Изменить схему:';
-$_['entry_main_category']    = 'Основная Категория:';
+$_['entry_sort_order']       = 'Порядок сортування:';
+$_['entry_category']         = 'Показувати в категоріях:';
+$_['entry_download']         = 'Завантаження:';
+$_['entry_related']          = 'Схожі галереї:';
+$_['entry_related_article']  = 'Схожі статті:';
+$_['entry_related_product']  = 'Супутні Товари:';
+$_['entry_layout']           = 'Змінити схему:';
+$_['entry_main_category']    = 'Основна Категорія:';
 $_['entry_seo_title']        = 'HTML-тег Title:';
 $_['entry_seo_h1']           = 'HTML-тег H1:';
 
 // Error
-$_['error_warning']          = 'Внимательно проверьте форму на ошибки!';
-$_['error_permission']       = 'У Вас нет прав для изменения галерей!';
-$_['error_name']             = 'Название галереи должно быть от 3 до 255 символов!';
+$_['error_warning']          = 'Уважно перевірте форму на помилки!';
+$_['error_permission']       = 'У Вас немає прав для зміни галерей!';
+$_['error_name']             = 'Назва галереї повинна бути від 3 до 255 символів!';
 ?>
