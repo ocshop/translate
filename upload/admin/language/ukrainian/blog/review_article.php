c0ae056f1b473af0ef18b765bf5dbbe0db6fd57b
ocshop <info@vilkov.info>
2015-05-06 15:38:34 +0300

    admin\review_article.php

--- upload/admin/language/ukrainian/blog/review_article.php
+++ upload/admin/language/ukrainian/blog/review_article.php
@@ -1,32 +1,37 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']     = 'Отзывы к Блогу';
+$_['heading_title']     = 'Відгуки до Блогу';
 
 // Text
-$_['text_success']      = 'Список отзывов обновлен!';
+$_['text_success']      = 'Список відгуків оновлено!';
 
 // Column
-$_['column_article']    = 'Статья';
+$_['column_article']    = 'Стаття';
 $_['column_author']     = 'Автор';
 $_['column_rating']     = 'Рейтинг';
 $_['column_status']     = 'Статус';
-$_['column_date_added'] = 'Добавлено';
-$_['column_action']     = 'Действие';
+$_['column_date_added'] = 'Додано';
+$_['column_action']     = 'Дія';
 
 // Entry
-$_['entry_article']     = 'Статья:';
+$_['entry_article']     = 'Стаття:';
 $_['entry_author']      = 'Автор:';
 $_['entry_rating']      = 'Рейтинг:';
 $_['entry_status']      = 'Статус:';
 $_['entry_text']        = 'Текст:';
-$_['entry_good']        = 'Хорошо';
-$_['entry_bad']         = 'Плохо';
+$_['entry_good']        = 'Добре';
+$_['entry_bad']         = 'Погано';
 $_['entry_date_added']  = 'Дата:';
 
 // Error
-$_['error_permission']  = 'У Вас нет прав для изменения отзывов!';
-$_['error_article']     = 'Требуется выбрать статью!';
-$_['error_author']      = 'Имя автора должно быть от 3 до 64 символов!';
-$_['error_text']        = 'Текст отзыва должен содержать хотя бы 1 символ!';
-$_['error_rating']      = 'Требуется установить рейтинг!';
+$_['error_permission']  = 'У Вас немає прав для зміни відгуків!';
+$_['error_article']     = 'Потрібно вибрати статтю!';
+$_['error_author']      = 'Ім’я автора повинно бути від 3 до 64 символів!';
+$_['error_text']        = 'Текст відгуку повинен містити хоча б 1 символ!';
+$_['error_rating']      = 'Потрібно встановити рейтинг!';
 ?>
