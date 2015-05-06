807c2b2fe1e652edc9c22c9c87841056853ac212
ocshop <info@vilkov.info>
2015-05-06 17:13:01 +0300

    admin\category.php

--- upload/admin/language/ukrainian/catalog/category.php
+++ upload/admin/language/ukrainian/catalog/category.php
@@ -1,44 +1,49 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']          = 'Категории';
+$_['heading_title']          = 'Категорії';
 
 // Text
-$_['text_success']           = 'Список категорий обновлен!';
-$_['text_default']           = 'Основной магазин';
-$_['text_image_manager']     = 'Управление изображениями';
-$_['text_browse']            = 'Просмотр файлов';
-$_['text_clear']             = 'Убрать изображение';
+$_['text_success']           = 'Список категорій оновлено!';
+$_['text_default']           = 'Основний магазин';
+$_['text_image_manager']     = 'Управління зображеннями';
+$_['text_browse']            = 'Перегляд файлів';
+$_['text_clear']             = 'Видалити зображення';
 
 // Column
-$_['column_name']            = 'Название категории';
-$_['column_sort_order']      = 'Порядок сортировки';
-$_['column_action']          = 'Действие';
+$_['column_name']            = 'Назва категорії';
+$_['column_sort_order']      = 'Порядок сортування';
+$_['column_action']          = 'Дія';
 
 // Entry
-$_['entry_name']             = 'Название категории:';
-$_['entry_meta_keyword'] 	 = 'META Keywords:<br/><span class="help">Мета-тег "Ключевые слова"</span>';
-$_['entry_meta_description'] = 'META Description:<br/><span class="help">Мета-тег "Описание"</span>';
-$_['entry_description']      = 'Описание Верх:';
-$_['entry_description_bottom']  = 'Описание Низ:';
-$_['entry_parent']         	 = 'Родительская категория:';
-$_['entry_filter']           = 'Фильтры:<br /><span class="help">(Автодополнение)</span>';
-$_['entry_store']            = 'Магазины:';
-$_['entry_keyword']          = 'SEO Keyword:<br /><span class="help">Заменяйте пробелы на знак "-". Значение этого поля должно быть уникальным на всю систему.</span>';
-$_['entry_image']            = 'Изображение категории:';
-$_['entry_top']              = 'Главное меню:<br/><span class="help">Показывать в главном меню (только для главных родительских категорий).</span>';
-$_['entry_column']           = 'Столбцы:<br/><span class="help">Количество столбцов в выпадающем меню категории (только для главных родительских категорий).</span>';
-$_['entry_sort_order']       = 'Порядок сортировки:';
+$_['entry_name']             = 'Назва категорії:';
+$_['entry_meta_keyword'] 	 = 'META Keywords:<br/><span class="help">Мета-тег "Ключові слова"</span>';
+$_['entry_meta_description'] = 'META Description:<br/><span class="help">Мета-тег "Опис"</span>';
+$_['entry_description']      = 'Опис Верх:';
+$_['entry_description_bottom']  = 'Опис Низ:';
+$_['entry_parent']         	 = 'Батьківська категорія:';
+$_['entry_filter']           = 'Фільтри:<br /><span class="help">(Автодоповнення)</span>';
+$_['entry_store']            = 'Магазини:';
+$_['entry_keyword']          = 'SEO Keyword:<br /><span class="help">Замінюйте пробіли на знак "-". Значення цього поля має бути унікальним на всю систему.</span>';
+$_['entry_image']            = 'Зображення категорії:';
+$_['entry_top']              = 'Головне меню:<br/><span class="help">Показувати в головному меню (тільки для головних батьківських категорій).</span>';
+$_['entry_column']           = 'Стовпці:<br/><span class="help">Кількість стовпців у випадаючому меню категорії (тільки для головних батьківських категорій).</span>';
+$_['entry_sort_order']       = 'Порядок сортування:';
 $_['entry_status']           = 'Статус:';
-$_['entry_layout']           = 'Изменить схему:';
-$_['entry_related_wb']          = 'Рекомендуемые товары:';
-$_['entry_related_article']     = 'Рекомендуем почитать:';
+$_['entry_layout']           = 'Змінити схему:';
+$_['entry_related_wb']          = 'Рекомендовані товари:';
+$_['entry_related_article']     = 'Рекомендуємо почитати:';
 $_['entry_seo_title']        = 'HTML-тег Title:';
 $_['entry_seo_h1']           = 'HTML-тег H1:';
 
 // Error
-$_['error_warning']          = 'Внимательно проверьте форму на ошибки!';
-$_['error_permission']       = 'У Вас нет прав для изменения категорий!';
-$_['error_name']             = 'Название категории должно быть от 2 до 32 символов!';
+$_['error_warning']          = 'Уважно перевірте форму на помилки!';
+$_['error_permission']       = 'У Вас немає прав для зміни категорій!';
+$_['error_name']             = 'Назва категорії має бути від 2 до 32 символів!';
 
 // ------
 $_['column_seo_keyword']     = 'SEO URL';
