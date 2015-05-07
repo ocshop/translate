2e4a356ed8292c54bdcd804c53c6113e780c183c
ocshop <info@vilkov.info>
2015-05-07 21:40:45 +0300

    admin\default.php

--- upload/admin/language/ukrainian/extension/default.php
+++ upload/admin/language/ukrainian/extension/default.php
@@ -1,20 +1,26 @@
 <?php
-$_['lang_openbay_new']              = 'Создать объявление';
-$_['lang_openbay_edit']             = 'Просмотр / Редактирование объявления';
-$_['lang_openbay_fix']              = 'Исправить ошибки';
-$_['lang_openbay_processing']       = 'Выполняется';
 
-$_['lang_amazonus_saved']           = 'Сохранено (не загружено)';
-$_['lang_amazon_saved']             = 'Сохранено (не загружено)';
-$_['lang_play_pending_new']         = 'Ожидает (новый)';
-$_['lang_play_pending_updated']     = 'Ожидает (обновленный)';
-$_['lang_play_warning']             = 'Предупреждения';
-$_['lang_play_pending_delete']      = 'Ожидает удаления';
-$_['lang_play_stock_updating']      = 'Обновление запасов';
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
 
-$_['lang_markets']                  = 'Рынки';
-$_['lang_bulk_btn']                 = 'Массовая загрузка eBay';
+$_['lang_openbay_new']              = 'Створити оголошення';
+$_['lang_openbay_edit']             = 'Перегляд / Редагування оголошення';
+$_['lang_openbay_fix']              = 'Виправити помилки';
+$_['lang_openbay_processing']       = 'Виконується';
+
+$_['lang_amazonus_saved']           = 'Збережено (не було завантажено)';
+$_['lang_amazon_saved']             = 'Збережено (не було завантажено)';
+$_['lang_play_pending_new']         = 'Чекає (новий)';
+$_['lang_play_pending_updated']     = 'Чекає (оновлений)';
+$_['lang_play_warning']             = 'Попередження';
+$_['lang_play_pending_delete']      = 'Чекає видалення';
+$_['lang_play_stock_updating']      = 'Оновлення запасів';
+
+$_['lang_markets']                  = 'Ринки';
+$_['lang_bulk_btn']                 = 'Масове завантаження eBay';
 
 $_['lang_marketplace']              = 'Marketplace';
 $_['lang_status']                   = 'Статус';
-$_['lang_option']                   = 'Вариант';
+$_['lang_option']                   = 'Варіант';
+?>
