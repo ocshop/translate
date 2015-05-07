e50b4c0249b892358415b7d1755714d8101afddd
ocshop <info@vilkov.info>
2015-05-08 01:46:47 +0300

    admin/tax_rate.php

--- upload/admin/language/ukrainian/localisation/tax_rate.php
+++ upload/admin/language/ukrainian/localisation/tax_rate.php
@@ -1,31 +1,36 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']        = 'Налоговые ставки';
+$_['heading_title']        = 'Податкові ставки';
 
 // Text
-$_['text_percent']         = 'Процент';
-$_['text_amount']          = 'Фиксированная величина';
-$_['text_success']         = 'Список налоговых ставок обновлен!';
+$_['text_percent']         = 'Відсоток';
+$_['text_amount']          = 'Фіксована величина';
+$_['text_success']         = 'Список податкових ставок оновлено!';
 
 // Column
-$_['column_name']          = 'Название налога';
+$_['column_name']          = 'Назва податку';
 $_['column_rate']          = 'Ставка';
 $_['column_type']          = 'Тип';
-$_['column_geo_zone']      = 'Географическая зона';
-$_['column_date_added']    = 'Добавлено';
-$_['column_date_modified'] = 'Изменено';
-$_['column_action']        = 'Действие';
+$_['column_geo_zone']      = 'Географічна зона';
+$_['column_date_added']    = 'Додано';
+$_['column_date_modified'] = 'Змінено';
+$_['column_action']        = 'Дія';
 
 // Entry
-$_['entry_name']           = 'Название:';
+$_['entry_name']           = 'Назва:';
 $_['entry_rate']           = 'Ставка:';
 $_['entry_type']           = 'Тип:';
-$_['entry_customer_group'] = 'Группа покупателей:';
-$_['entry_geo_zone']       = 'Географическая зона:';
+$_['entry_customer_group'] = 'Група покупців:';
+$_['entry_geo_zone']       = 'Географічна зона:';
 
 // Error
-$_['error_permission']     = 'У Вас нет прав для изменения налоговых ставок!';
-$_['error_tax_rule']       = 'Налог не может быть исключен, поскольку он прикреплен к %s классам!';
-$_['error_name']           = 'Название должно быть от 3 до 32 символов!';
-$_['error_rate']           = 'Требуется ввести ставку!';
+$_['error_permission']     = 'У Вас немає прав для зміни податкових ставок!';
+$_['error_tax_rule']       = 'Податок не може бути виключений, оскільки він прикріплений до %s класам!';
+$_['error_name']           = 'Назва має бути від 3 до 32 символів!';
+$_['error_rate']           = 'Потрібно ввести ставку!';
 ?>
