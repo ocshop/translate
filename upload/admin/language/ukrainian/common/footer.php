c15688ccd08d9d313ade1cc699b7264bb053556d
ocshop <info@vilkov.info>
2015-05-07 15:12:31 +0300

    admin\footer.php

--- upload/admin/language/ukrainian/common/footer.php
+++ upload/admin/language/ukrainian/common/footer.php
@@ -1,4 +1,9 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_footer'] = '<a href="http://ocshop.info">OCSHOP.CMS</a> &copy; 2009-' . date('Y') . ' <a href="http://forum.ocshop.info">Форум Поддержки</a><br />All Rights Reserved. Версия %s';
+$_['text_footer'] = '<a href="http://ocshop.info">OCSHOP.CMS</a> &copy; 2009-' . date('Y') . ' <a href="http://forum.ocshop.info">Форум Підтримки</a><br />All Rights Reserved. Версія %s';
 ?>
