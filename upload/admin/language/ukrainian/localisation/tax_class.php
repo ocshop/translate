8ec1baa461edd1b47137e0494273ce9fddfb6f17
ocshop <info@vilkov.info>
2015-05-08 01:31:13 +0300

    admin\tax_class.php

--- upload/admin/language/ukrainian/localisation/tax_class.php
+++ upload/admin/language/ukrainian/localisation/tax_class.php
@@ -1,28 +1,33 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']     = 'Налоговые ставки';
+$_['heading_title']     = 'Податкові ставки';
 
 // Text
-$_['text_shipping']     = 'Адрес доставки';
-$_['text_payment']      = 'Платёжный адрес';
-$_['text_store']        = 'Адрес магазина';
-$_['text_success']      = 'Список налоговых ставок обновлен!';
+$_['text_shipping']     = 'Адреса доставки';
+$_['text_payment']      = 'Платіжна адреса';
+$_['text_store']        = 'Адреса магазину';
+$_['text_success']      = 'Список податкових ставок оновлено!';
 
 // Column
-$_['column_title']      = 'Налоговые ставки';
-$_['column_action']     = 'Действие';
+$_['column_title']      = 'Податкові ставки';
+$_['column_action']     = 'Дія';
 
 // Entry
-$_['entry_title']       = 'Налоговая ставка:';
-$_['entry_description'] = 'Описание:';
-$_['entry_rate']        = 'Налоговая ставка:';
-$_['entry_based']       = 'Основана на:';
-$_['entry_geo_zone']    = 'Географическая зона:';
-$_['entry_priority']    = 'Приоритет:';
+$_['entry_title']       = 'Податкова ставка:';
+$_['entry_description'] = 'Опис:';
+$_['entry_rate']        = 'Податкова ставка:';
+$_['entry_based']       = 'Заснована на:';
+$_['entry_geo_zone']    = 'Географічна зона:';
+$_['entry_priority']    = 'Пріоритет:';
 
 // Error
-$_['error_permission']  = 'У Вас нет прав для изменения налоговых ставок!';
-$_['error_title']       = 'Название налога должно быть от 3 до 32 символов!';
-$_['error_description'] = 'Описание должно быть от 3 до 255 символов!';
-$_['error_product']     = 'Налог не может быть исключен, поскольку он прикреплен к %s товару(ам)';
+$_['error_permission']  = 'У Вас немає прав для зміни податкових ставок!';
+$_['error_title']       = 'Назва податку має бути від 3 до 32 символів!';
+$_['error_description'] = 'Опис повинен бути від 3 до 255 символів!';
+$_['error_product']     = 'Податок не може бути виключений, оскільки він прикріплений до %s товару(ів)';
 ?>
