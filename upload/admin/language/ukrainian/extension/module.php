72f5752e9861a52b93768b59f5200c6210d54100
ocshop <info@vilkov.info>
2015-05-07 21:55:50 +0300

    admin\module.php

--- upload/admin/language/ukrainian/extension/module.php
+++ upload/admin/language/ukrainian/extension/module.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']     = 'Модули';
+$_['heading_title']     = 'Модулі';
 
 // Text
-$_['text_install']      = 'Установить';
-$_['text_uninstall']    = 'Удалить';
+$_['text_install']      = 'Встановити';
+$_['text_uninstall']    = 'Видалити';
 
 // Column
-$_['column_name']       = 'Название модуля';
-$_['column_action']     = 'Действие';
+$_['column_name']       = 'Назва модуля';
+$_['column_action']     = 'Дія';
 
 // Error
-$_['error_permission']  = 'У Вас нет прав для управления модулями!';
+$_['error_permission']  = 'У Вас немає прав для управління модулями!';
 ?>
