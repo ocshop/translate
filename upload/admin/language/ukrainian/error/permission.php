65ca31ab5296aa8d38d3a61bfc4a5e48bb4242e5
ocshop <info@vilkov.info>
2015-05-07 21:33:43 +0300

    admin\permission.php

--- upload/admin/language/ukrainian/error/permission.php
+++ upload/admin/language/ukrainian/error/permission.php
@@ -1,7 +1,12 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title'] = 'Доступ запрещен!';
+$_['heading_title'] = 'Доступ заборонено!';
 
 // Text
-$_['text_permission'] = 'У Вас нет прав для доступа к этой странице. Если она Вам нужна, обратитесь к администратору.';
+$_['text_permission'] = 'У Вас немає прав для доступу до цієї сторінки. Якщо вона Вам потрібна, зверніться до адміністратора.';
 ?>
