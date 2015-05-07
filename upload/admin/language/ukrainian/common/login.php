9e4a0f3ce00d6ef05f6da6caf461608f6d795bb0
ocshop <info@vilkov.info>
2015-05-07 17:07:10 +0300

    admin\login.php

--- upload/admin/language/ukrainian/common/login.php
+++ upload/admin/language/ukrainian/common/login.php
@@ -1,20 +1,25 @@
 <?php
-// header
-$_['heading_title']  = 'Авторизация';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+// Heading
+$_['heading_title']  = 'Авторизація';
 
 // Text
-$_['text_heading']   = 'Авторизация';
-$_['text_login']     = 'Введите логин и пароль';
-$_['text_forgotten'] = 'Забыли пароль?';
+$_['text_heading']   = 'Авторизація';
+$_['text_login']     = 'Введіть логін і пароль';
+$_['text_forgotten'] = 'Забули пароль?';
 
 // Entry
-$_['entry_username'] = 'Логин';
+$_['entry_username'] = 'Логін';
 $_['entry_password'] = 'Пароль';
 
 // Button
-$_['button_login']   = 'Войти';
+$_['button_login']   = 'Ввійти';
 
 // Error
-$_['error_login']    = 'Такой логин и/или пароль не существует!';
-$_['error_token']    = 'Неправильная токен-сессия. Авторизуйтесь снова.';
+$_['error_login']    = 'Такий логін і/або пароль не існує!';
+$_['error_token']    = 'Неправильна токен-сесія. авторизуйтесь знову.';
 ?>
