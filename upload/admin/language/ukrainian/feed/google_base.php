07fabe369c50cc77f2a778b5a03233b46ed0f6dc
ocshop <info@vilkov.info>
2015-05-07 22:03:03 +0300

    admin\google_base.php

--- upload/admin/language/ukrainian/feed/google_base.php
+++ upload/admin/language/ukrainian/feed/google_base.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']    = 'Google Base';
 
 // Text
-$_['text_feed']        = 'Каналы продвижения';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_feed']        = 'Канали просування';
+$_['text_success']     = 'Налаштування модуля оновлені!';
 
 // Entry
 $_['entry_status']     = 'Статус:';
-$_['entry_data_feed']  = 'URL канала:';
+$_['entry_data_feed']  = 'URL каналу:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
