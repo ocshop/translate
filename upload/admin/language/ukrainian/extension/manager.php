f489059311e4e2dbbe5b11ebd7999f354f71fd66
ocshop <info@vilkov.info>
2015-05-07 21:44:44 +0300

    admin\manager.php

--- upload/admin/language/ukrainian/extension/manager.php
+++ upload/admin/language/ukrainian/extension/manager.php
@@ -1,12 +1,17 @@
 <?php
-// Heading 
-$_['heading_title']    = 'Управление модулями';
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
+// Heading
+$_['heading_title']    = 'Управління модулями';
 
 // Text
-$_['text_success']     = 'Модуль успешно установлен!';
+$_['text_success']     = 'Модуль успішно встановлено!';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
-$_['error_upload']     = 'Укажите файл для загрузки!';
-$_['error_filetype']   = 'Недопустимый тип файла!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
+$_['error_upload']     = 'Вкажіть файл для завантаження!';
+$_['error_filetype']   = 'Неприпустимий тип файлу!';
 ?>
