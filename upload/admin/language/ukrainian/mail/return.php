42b470b2c7fa4fe52ad076516d78288890f5cfea
ocshop <info@vilkov.info>
2015-05-08 02:29:22 +0300

    admin\return.php

--- upload/admin/language/ukrainian/mail/return.php
+++ upload/admin/language/ukrainian/mail/return.php
@@ -1,9 +1,14 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_subject']       = '%s - возврат обновлен %s';
-$_['text_return_id']     = '№ возврата:';
-$_['text_date_added']    = 'Дата возврата:';
-$_['text_return_status'] = 'Актуальное состояние Вашего возврата:';
-$_['text_comment']       = 'Комментарии к Вашему возврату:';
-$_['text_footer']        = 'Если у Вас есть какие-либо вопросы, ответьте на это сообщение.';
+$_['text_subject']       = '%s - повернення оновлено %s';
+$_['text_return_id']     = '№ повернення:';
+$_['text_date_added']    = 'Дата повернення:';
+$_['text_return_status'] = 'Актуальний стан Вашого повернення:';
+$_['text_comment']       = 'Коментарі до Вашого повернення:';
+$_['text_footer']        = 'Якщо у Вас є які-небудь питання, дайте відповідь на це повідомлення.';
 ?>
