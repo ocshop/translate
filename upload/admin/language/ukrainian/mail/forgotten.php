20ec036213a3a8f1a4a42bf2bb13436da9e7f805
ocshop <info@vilkov.info>
2015-05-08 02:24:49 +0300

    admin\forgotten.php

--- upload/admin/language/ukrainian/mail/forgotten.php
+++ upload/admin/language/ukrainian/mail/forgotten.php
@@ -1,7 +1,12 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Text
-$_['text_subject']  = '%s - Изменение пароля';
-$_['text_greeting'] = 'Новый пароль для %s.';
-$_['text_change']   = 'Что бы изменить пароль нажмите сслку:';
-$_['text_ip']       = 'IP этого запроса: %s';
+$_['text_subject']  = '%s - Зміна пароля';
+$_['text_greeting'] = 'Новий пароль для %s.';
+$_['text_change']   = 'Що б змінити пароль натисніть посилання:';
+$_['text_ip']       = 'IP цього запиту: %s';
 ?>
