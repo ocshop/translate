32d96ffbc819aaa07abe5f87736116105e30808d
ocshop <info@vilkov.info>
2015-05-07 21:42:56 +0300

    admin\feed.php

--- upload/admin/language/ukrainian/extension/feed.php
+++ upload/admin/language/ukrainian/extension/feed.php
@@ -1,16 +1,21 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']  = 'Каналы продвижения';
+$_['heading_title']  = 'Канали просування';
 
 // Text
-$_['text_install']   = 'Установить';
-$_['text_uninstall'] = 'Удалить';
+$_['text_install']   = 'Встановити';
+$_['text_uninstall'] = 'Видалити';
 
 // Column
-$_['column_name']    = 'Название канала';
+$_['column_name']    = 'Назва каналу';
 $_['column_status']  = 'Статус';
-$_['column_action']  = 'Действие';
+$_['column_action']  = 'Дія';
 
 // Error
-$_['error_permission']  = 'У Вас нет прав для изменения канала товаров!';
+$_['error_permission']  = 'У Вас немає прав для зміни каналів!';
 ?>
