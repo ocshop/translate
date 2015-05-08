c30193ae8bb933b472324742740f2daa41793b3d
ocshop <info@vilkov.info>
2015-05-08 03:31:59 +0300

    admin\bestseller.php

--- upload/admin/language/ukrainian/module/bestseller.php
+++ upload/admin/language/ukrainian/module/bestseller.php
@@ -1,24 +1,29 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']       = 'Хиты продаж';
+$_['heading_title']       = 'Хіти продажу';
 
 // Text
-$_['text_module']         = 'Модули';
-$_['text_success']        = 'Модуль "Хиты продаж" успешно обновлен!';
-$_['text_content_top']    = 'Верх страницы';
-$_['text_content_bottom'] = 'Низ страницы';
-$_['text_column_left']    = 'Левая колонка';
-$_['text_column_right']   = 'Правая колонка';
+$_['text_module']         = 'Модулі';
+$_['text_success']        = 'Модуль успішно оновлено!';
+$_['text_content_top']    = 'Верх сторінки';
+$_['text_content_bottom'] = 'Низ сторінки';
+$_['text_column_left']    = 'Ліва колонка';
+$_['text_column_right']   = 'Права колонка';
 
 // Entry
-$_['entry_limit']         = 'Лимит:';
-$_['entry_image']         = 'Изображение (Ширина x Высота):';
-$_['entry_layout']        = 'Страница:';
-$_['entry_position']      = 'Расположение:';
+$_['entry_limit']         = 'Ліміт:';
+$_['entry_image']         = 'Зображення (Ширина x Висота):';
+$_['entry_layout']        = 'Схема:';
+$_['entry_position']      = 'Розташування:';
 $_['entry_status']        = 'Статус:';
-$_['entry_sort_order']    = 'Порядок сортировки:';
+$_['entry_sort_order']    = 'Порядок сортування:';
 
 // Error
-$_['error_permission']    = 'У Вас нет прав для изменения модуля "Хиты продаж"!';
-$_['error_image']         = 'Введите рамеры изображения!';
+$_['error_permission']    = 'У Вас немає прав для зміни модуля!';
+$_['error_image']         = 'Введіть розміри зображення!';
 ?>
