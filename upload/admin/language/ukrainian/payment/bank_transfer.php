af9668f0bb31c1d3015b60994eaafdebb8dc8029
ocshop <info@vilkov.info>
2015-05-08 18:15:45 +0300

    admin\bank_transfer.php

--- upload/admin/language/ukrainian/payment/bank_transfer.php
+++ upload/admin/language/ukrainian/payment/bank_transfer.php
@@ -1,20 +1,25 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']      = 'Банковский перевод';
+$_['heading_title']      = 'Банківський переказ';
 
 // Text
 $_['text_payment']       = 'Оплата';
-$_['text_success']       = 'Настройки модуля обновлены!';
+$_['text_success']       = 'Налаштування модуля оновлені!';
 
 // Entry
-$_['entry_bank']         = 'Инструкция по переводу:';
-$_['entry_total']        = 'Нижняя граница:<br /><span class="help">Минимальная сумма заказа. Ниже этой суммы метод будет недоступен.</span>';
-$_['entry_order_status'] = 'Статус заказа после оплаты:';
-$_['entry_geo_zone']     = 'Географическая зона:';
+$_['entry_bank']         = 'Інструкція з переказу:';
+$_['entry_total']        = 'Нижня межа:<br /><span class="help">Мінімальна сума замовлення. Нижче цієї суми метод буде недоступний.</span>';
+$_['entry_order_status'] = 'Статус замовлення після оплати:';
+$_['entry_geo_zone']     = 'Географічна зона:';
 $_['entry_status']       = 'Статус:';
-$_['entry_sort_order']   = 'Порядок сортировки:';
+$_['entry_sort_order']   = 'Порядок сортування:';
 
 // Error
-$_['error_permission']   = 'У Вас нет прав для управления этим модулем!';
-$_['error_bank']         = 'Необходима инструкция по переводу!';
+$_['error_permission']   = 'У Вас немає прав для управління цим модулем!';
+$_['error_bank']         = 'Необхідна інструкція з переказу!';
 ?>
