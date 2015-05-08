5b172a44702d7ef393cef7d506a8fe0be7c4dc76
ocshop <info@vilkov.info>
2015-05-08 19:06:26 +0300

    admin/moneybookers.php

--- upload/admin/language/ukrainian/payment/moneybookers.php
+++ upload/admin/language/ukrainian/payment/moneybookers.php
@@ -1,26 +1,31 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']		      = 'Moneybookers';
 
 // Text
-$_['text_payment']		      = 'Payment';
-$_['text_success']		      = 'Success: You have modified the Moneybookers details.';
+$_['text_payment']       	  = 'Оплата';
+$_['text_success']       	  = 'Налаштування модуля оновлені!';
 $_['text_moneybookers']	      = '<a href="https://www.moneybookers.com/partners/?p=OpenCart" target="_blank"><img src="view/image/payment/moneybookers.png" alt="Moneybookers" title="Moneybookers" style="border: 1px solid #EEEEEE;" /></a>';
 	
 // Entry					
 $_['entry_email']		      = 'E-Mail:';
 $_['entry_secret']		      = 'Secret:';
-$_['entry_total']             = 'Нижняя граница:<br /><span class="help">Минимальная сумма заказа. Ниже этой суммы метод будет недоступен.</span>';
-$_['entry_order_status']      = 'Order Status:';
-$_['entry_pending_status']    = 'Pending Status :';
-$_['entry_canceled_status']   = 'Canceled Status:';
-$_['entry_failed_status']     = 'Failed Status:';
-$_['entry_chargeback_status'] = 'Chargeback Status:';
-$_['entry_geo_zone']          = 'Geo Zone:';
-$_['entry_status']            = 'Status:';
-$_['entry_sort_order']        = 'Sort Order:';
+$_['entry_total']        	  = 'Нижня межа:<br /><span class="help">Мінімальна сума замовлення. Нижче цієї суми метод буде недоступний.</span>';
+$_['entry_order_status'] 	  = 'Статус замовлення після оплати:';
+$_['entry_pending_status']    = 'Статус в очікуванні:';
+$_['entry_canceled_status']   = 'Статус скасовано:';
+$_['entry_failed_status']     = 'Статус не вдалося:';
+$_['entry_chargeback_status'] = 'Статус платежа:';
+$_['entry_geo_zone']     	  = 'Географічна зона:';
+$_['entry_status']       	  = 'Статус:';
+$_['entry_sort_order']   	  = 'Порядок сортування:';
 
 // Error
-$_['error_permission']        = 'У Вас нет прав для управления этим модулем!';
-$_['error_email']		      = 'Не заполнено поле E-Mail!';
+$_['error_permission']   	  = 'У Вас немає прав для управління цим модулем!';
+$_['error_email']		      = 'Не заповнене поле E-Mail!';
 ?>
