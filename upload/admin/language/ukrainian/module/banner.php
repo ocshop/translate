40f6a0b0bc96b135426ab854ccaf017191fa0b5d
ocshop <info@vilkov.info>
2015-05-08 03:29:10 +0300

    admin\banner.php

--- upload/admin/language/ukrainian/module/banner.php
+++ upload/admin/language/ukrainian/module/banner.php
@@ -1,24 +1,29 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']       = 'Баннер';
+$_['heading_title']       = 'Банер';
 
 // Text
-$_['text_module']         = 'Модули';
-$_['text_success']        = 'Модуль баннера успешно обновлен!';
-$_['text_content_top']    = 'Верх страницы';
-$_['text_content_bottom'] = 'Низ страницы';
-$_['text_column_left']    = 'Левая колонка';
-$_['text_column_right']   = 'Правая колонка';
+$_['text_module']         = 'Модулі';
+$_['text_success']        = 'Модуль успішно оновлено!';
+$_['text_content_top']    = 'Верх сторінки';
+$_['text_content_bottom'] = 'Низ сторінки';
+$_['text_column_left']    = 'Ліва колонка';
+$_['text_column_right']   = 'Права колонка';
 
 // Entry
-$_['entry_banner']        = 'Баннер:';
-$_['entry_dimension']     = 'Размеры (Ширина x Высота):';
-$_['entry_layout']        = 'Страница:';
-$_['entry_position']      = 'Расположение:';
+$_['entry_banner']        = 'Банер:';
+$_['entry_dimension']     = 'Розміри (Ширина x Висота):';
+$_['entry_layout']        = 'Схема:';
+$_['entry_position']      = 'Розташування:';
 $_['entry_status']        = 'Статус:';
-$_['entry_sort_order']    = 'Порядок сортировки:';
+$_['entry_sort_order']    = 'Порядок сортування:';
 
 // Error
-$_['error_permission']    = 'У Вас нет прав для изменения модуля баннера!';
-$_['error_dimension']     = 'Введите размеры баннера!';
+$_['error_permission']    = 'У Вас немає прав для зміни модуля!';
+$_['error_dimension']     = 'Введіть розміри банера!';
 ?>
