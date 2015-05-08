0625294f9ffe0cc7a4c17638d93e85c7a9867725
ocshop <info@vilkov.info>
2015-05-08 03:35:24 +0300

    admin/blockhtml.php

--- upload/admin/language/ukrainian/module/blockhtml.php
+++ upload/admin/language/ukrainian/module/blockhtml.php
@@ -1,26 +1,31 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']       	= 'Модуль HTML'; 
 
 // Text
-$_['text_module']         	= 'Модули';
-$_['text_success']        	= 'Модуль HTML успешно обновлен!';
-$_['text_content_top']    	= 'Содержание шапки';
-$_['text_content_bottom'] 	= 'Содержание подвала';
-$_['text_column_left']    	= 'Левая колонка';
-$_['text_column_right']   	= 'Правая колонка';
+$_['text_module']         	= 'Модулі';
+$_['text_success']        	= 'Модуль успішно оновлено!';
+$_['text_content_top']    	= 'Верх сторінки';
+$_['text_content_bottom'] 	= 'Низ сторінки';
+$_['text_column_left']    	= 'Ліва колонка';
+$_['text_column_right']   	= 'Права колонка';
 
 // Entry
 $_['entry_html'] 			= 'Текст HTML';
-$_['entry_yes'] 			= 'Да';
-$_['entry_no'] 				= 'Нет';
-$_['entry_title'] 			= 'Название <p class="help">Текст заголовка</p>';
-$_['entry_header'] 			= 'Заголовок <p class="help">Показывать заголовок</p>';
+$_['entry_yes'] 			= 'Так';
+$_['entry_no'] 				= 'Ні';
+$_['entry_title'] 			= 'Назва <p class="help">Текст заголовка</p>';
+$_['entry_header'] 			= 'Заголовок <p class="help">Показувати заголовок</p>';
 $_['entry_layout']        	= 'Схема:';
-$_['entry_position']      	= 'Расположение:';
+$_['entry_position']      	= 'Розташування:';
 $_['entry_status']        	= 'Статус:';
-$_['entry_sort_order']    	= 'Порядок сортировки:';
+$_['entry_sort_order']    	= 'Порядок сортування:';
 
 // Error
-$_['error_permission']    	= 'У Вас нет прав для изменения модуля HTML!';
+$_['error_permission']    = 'У Вас немає прав для зміни модуля!';
 ?>
