dc01a8a9ddfe875659b5623f9def8babcc633125
ocshop <info@vilkov.info>
2015-05-08 04:34:18 +0300

    admin\google_talk.php

--- upload/admin/language/ukrainian/module/google_talk.php
+++ upload/admin/language/ukrainian/module/google_talk.php
@@ -1,23 +1,28 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']       = 'Google Talk';
 
 // Text
-$_['text_module']         = 'Модули';
-$_['text_success']        = 'Настройки модуля обновлены!';
-$_['text_content_top']    = 'Верх страницы';
-$_['text_content_bottom'] = 'Низ страницы';
-$_['text_column_left']    = 'Левая колонка';
-$_['text_column_right']   = 'Правая колонка';
+$_['text_module']         = 'Модулі';
+$_['text_success']        = 'Модуль успішно оновлено!';
+$_['text_content_top']    = 'Верх сторінки';
+$_['text_content_bottom'] = 'Низ сторінки';
+$_['text_column_left']    = 'Ліва колонка';
+$_['text_column_right']   = 'Права колонка';
 
 // Entry
-$_['entry_code']          = 'Код:<br /><span class="help">Перейдите на страницу <a onclick="window.open(\'http://www.google.com/talk/service/badge/New\');"><u>Создание виджета Google Talk</u></a>. Скопируйте и вставьте сгенерированный код в текстовое поле.</span>';
-$_['entry_layout']        = 'Страница:';
-$_['entry_position']      = 'Расположение:';
+$_['entry_code']          = 'Код:<br /><span class="help">Перейдіть на сторінку <a onclick="window.open(\'http://www.google.com/talk/service/badge/New\');"><u>Створення віджета Google Talk</u></a>. Скопіюйте та вставте згенерований код в текстове поле.</span>';
+$_['entry_layout']        = 'Схема:';
+$_['entry_position']      = 'Розташування:';
 $_['entry_status']        = 'Статус:';
-$_['entry_sort_order']    = 'Порядок сортировки:';
+$_['entry_sort_order']    = 'Порядок сортування:';
 
 // Error
-$_['error_permission']    = 'У Вас нет прав для управления этим модулем!';
-$_['error_code']          = 'Необходим код';
+$_['error_permission']    = 'У Вас немає прав для зміни модуля!';
+$_['error_code']          = 'Потрібен код';
 ?>
