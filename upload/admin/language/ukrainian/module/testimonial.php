3cb5a17d5a265d5fc8c4c103983527fd8e4ac96a
ocshop <info@vilkov.info>
2015-05-08 04:55:00 +0300

    admin/testimonial.php

--- upload/admin/language/ukrainian/module/testimonial.php
+++ upload/admin/language/ukrainian/module/testimonial.php
@@ -1,34 +1,39 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    			= 'Отзывы о Магазине';
+$_['heading_title']    				= 'Відгуки про Магазин';
 
 // Text
-$_['text_module']         		= 'Модули';
-$_['text_success']        		= 'Настройки модуля обновлены!';
-$_['text_content_top']   		= 'Содержание шапки';
-$_['text_content_bottom'] 		= 'Содержание подвала';
-$_['text_column_left']    		= 'Левая колонка';
-$_['text_column_right']   		= 'Правая колонка';
-$_['text_edit_testimonials']   	= 'Редактировать отзывы';
+$_['text_module']         			= 'Модулі';
+$_['text_success']        			= 'Модуль успішно оновлено!';
+$_['text_content_top']    			= 'Верх сторінки';
+$_['text_content_bottom'] 			= 'Низ сторінки';
+$_['text_column_left']    			= 'Ліва колонка';
+$_['text_column_right']   			= 'Права колонка';
+$_['text_edit_testimonials']   		= 'Редагувати відгуки';
 
 // Entry
-$_['entry_layout']        		= 'Схема:';
-$_['entry_position']      		= 'Расположение:';
-$_['entry_status']        		= 'Статус:';
-$_['entry_sort_order']    		= 'Порядок сортировки:';
-$_['entry_limit']      		  	= 'Количество:';
-$_['entry_title']       	  	= 'Заголовок:';
-$_['entry_character_limit']     	= 'Сколько символов показывать (пусто - показать все):';
-$_['entry_admin_approved']      	= 'Публикация после одобрения Администратором:';
-$_['entry_default_rating']      	= 'Рейтинг по умолчанию:';
-$_['entry_good']         		= 'Отлично';
-$_['entry_bad']           		= 'Плохо';
-$_['entry_random']      		= 'Случайный порядок:';
-$_['entry_send_to_admin']      	= 'Отправить отзыв Администратору по E-Mail:';
-$_['entry_all_page_limit']      	= 'Количество отзывов на странице Все отзывы:';
+$_['entry_layout']        			= 'Схема:';
+$_['entry_position']      			= 'Розташування:';
+$_['entry_status']        			= 'Статус:';
+$_['entry_sort_order']    			= 'Порядок сортування:';
+$_['entry_limit']      		  		= 'Кількість:';
+$_['entry_title']       	  		= 'Заголовок:';
+$_['entry_character_limit']     	= 'Скільки символів показувати (порожньо - показати все):';
+$_['entry_admin_approved']      	= 'Публікація після схвалення Адміністратором:';
+$_['entry_default_rating']      	= 'Рейтинг за замовчуванням:';
+$_['entry_good']         			= 'Відмінно';
+$_['entry_bad']           			= 'Погано';
+$_['entry_random']      			= 'Випадковий порядок:';
+$_['entry_send_to_admin']      		= 'Надіслати відгук Адміністратору по E-Mail:';
+$_['entry_all_page_limit']      	= 'Кількість відгуків на сторінці Усі відгуки:';
 
 
 
 // Error
-$_['error_permission'] 			= 'У Вас нет прав для изменения модуля Отзывы о Магазине!';
+$_['error_permission']    			= 'У Вас немає прав для зміни модуля!';
 ?>
