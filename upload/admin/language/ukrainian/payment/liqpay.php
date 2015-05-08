f673c7394f38d3f3de8eec80b439e94cd1489fd2
ocshop <info@vilkov.info>
2015-05-08 18:29:01 +0300

    admin\liqpay.php

--- upload/admin/language/ukrainian/payment/liqpay.php
+++ upload/admin/language/ukrainian/payment/liqpay.php
@@ -1,10 +1,15 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']      = 'LIQPAY';
 
 // Text
 $_['text_payment']       = 'Оплата';
-$_['text_success']       = 'Настройки модуля обновлены!';
+$_['text_success']       = 'Налаштування модуля оновлені!';
 $_['text_pay']           = 'LIQPAY';
 $_['text_card']          = 'Credit Card';
 
@@ -12,14 +17,14 @@ $_['text_card']          = 'Credit Card';
 $_['entry_merchant']     = 'Merchant ID:';
 $_['entry_signature']    = 'Signature:';
 $_['entry_type']         = 'Type:';
-$_['entry_total']        = 'Нижняя граница:<br /><span class="help">Минимальная сумма заказа. Ниже этой суммы метод будет недоступен.</span>';
-$_['entry_order_status'] = 'Статус заказа после оплаты:';
-$_['entry_geo_zone']     = 'Географическая зона:';
+$_['entry_total']        = 'Нижня межа:<br /><span class="help">Мінімальна сума замовлення. Нижче цієї суми метод буде недоступний.</span>';
+$_['entry_order_status'] = 'Статус замовлення після оплати:';
+$_['entry_geo_zone']     = 'Географічна зона:';
 $_['entry_status']       = 'Статус:';
-$_['entry_sort_order']   = 'Порядок сортировки:';
+$_['entry_sort_order']   = 'Порядок сортування:';
 
 // Error
-$_['error_permission']   = 'У Вас нет прав для управления этим модулем!';
-$_['error_merchant']     = 'Неверный ID магазина (Merchant ID)!';
-$_['error_signature']    = 'Отсутствует сигнатура!';
+$_['error_permission']   = 'У Вас немає прав для управління цим модулем!';
+$_['error_merchant']     = 'Невірний ID магазину (Merchant ID)!';
+$_['error_signature']    = 'Відсутня сигнатура!';
 ?>
