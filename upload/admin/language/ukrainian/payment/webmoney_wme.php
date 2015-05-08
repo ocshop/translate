1902f9968ee8c2dc556cf43aa6082226a977030e
ocshop <info@vilkov.info>
2015-05-08 19:21:09 +0300

    admin\webmoney_wme.php

--- upload/admin/language/ukrainian/payment/webmoney_wme.php
+++ upload/admin/language/ukrainian/payment/webmoney_wme.php
@@ -1,27 +1,32 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']      	= 'Webmoney WME';
 
 // Text 
-$_['text_payment']      	= 'Оплаты';
-$_['text_success']       	= 'Настройки модуля обновлены!';
-$_['text_webmoney_wme'] 		= '<a onclick="window.open(\'http://webmoney.ru\');"><img src="/image/payment/webmoney_wme.gif" alt="Webmoney" title="Webmoney" style="border: 1px solid #EEEEEE;" height="25px" /></a>';
+$_['text_payment']       	= 'Оплата';
+$_['text_success']       	= 'Налаштування модуля оновлені!';
+$_['text_webmoney_wme'] 	= '<a onclick="window.open(\'http://webmoney.ru\');"><img src="/image/payment/webmoney_wme.gif" alt="Webmoney" title="Webmoney" style="border: 1px solid #EEEEEE;" height="25px" /></a>';
 
 // Entry
-$_['entry_merch_e']      	= 'Номер кошелька E:';
+$_['entry_merch_e']      	= 'Номер гаманця E:';
 $_['entry_secret_key']      = 'Secret Key:';
 $_['entry_result_url']     	= 'Result URL:';
 $_['entry_success_url']     = 'Success URL:';
 $_['entry_fail_url']      	= 'Fail URL :';
-$_['entry_order_status'] 	= 'Статус заказа после оплаты:';
-$_['entry_geo_zone']     	= 'Гео. Зона:';
-$_['entry_status']       	= 'Состояние:';
-$_['entry_sort_order']   	= 'Сортировка:';
-$_['entry_donate_me']   	= 'Благодарить сюда:';
+$_['entry_order_status'] 	= 'Статус замовлення після оплати:';
+$_['entry_geo_zone']     	= 'Географічна зона:';
+$_['entry_status']       	= 'Статус:';
+$_['entry_sort_order']   	= 'Порядок сортування:';
+$_['entry_donate_me']   	= 'Дякувати сюди:';
 
 // Error
-$_['error_permission']   	= 'У Вас нет прав для изменения модуля!';
-$_['error_merch_e']        	= 'Неверный номер кошелька!';
-$_['error_secret_key']    = 'Отсутствует Merchant Key';
+$_['error_permission']   	= 'У Вас немає прав для управління цим модулем!';
+$_['error_merch_e']        	= 'Невірний номер гаманця!';
+$_['error_secret_key']    	= 'Відсутній Merchant Key';
 
 ?>
