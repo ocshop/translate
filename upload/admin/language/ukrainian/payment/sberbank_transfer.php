b0df51f1e326393dc5b2b47330fe61456ae1b9bf
ocshop <info@vilkov.info>
2015-05-08 19:17:51 +0300

    admin\sberbank_transfer.php

--- upload/admin/language/ukrainian/payment/sberbank_transfer.php
+++ upload/admin/language/ukrainian/payment/sberbank_transfer.php
@@ -1,25 +1,30 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']          = 'Квитанция Сбербанка РФ';
+$_['heading_title']          = 'Квитанція Ощадбанку';
 
 // Text
 $_['text_payment']           = 'Оплата';
-$_['text_success']           = 'Настройки модуля обновлены!';
+$_['text_success']           = 'Налаштування модуля оновлені!';
 $_['text_sberbank_transfer'] = '<a onclick="window.open(\'http://www.sbrf.ru\');"><img src="view/image/payment/sberbank_transfer.png" alt="Сбербанк" title="Сбербанк" style="border: 1px solid #EEEEEE;" /></a>';
 
 // Entry
-$_['entry_bank']             = 'Наименование получателя платежа:';
-$_['entry_inn']              = 'ИНН:';
-$_['entry_rs']               = 'Номер счета получателя платежа:';
-$_['entry_bankuser']         = 'Наименование банка получателя платежа:';
+$_['entry_bank']             = 'Найменування одержувача платежу:';
+$_['entry_inn']              = 'ІПН:';
+$_['entry_rs']               = 'Номер рахунку отримувача платежу:';
+$_['entry_bankuser']         = 'Найменування банку одержувача платежу:';
 $_['entry_bik']              = 'БИК:';
-$_['entry_ks']               = 'Номер кор./сч. банка получателя платежа:';
-$_['entry_order_status']     = 'Статус заказа после оплаты:';
-$_['entry_geo_zone']         = 'Географическая зона:';
-$_['entry_status']           = 'Статус:';
-$_['entry_sort_order']       = 'Порядок сортировки:';
+$_['entry_ks']               = 'Номер кор. / рах. банку одержувача платежу:';
+$_['entry_order_status']     = 'Статус замовлення після оплати:';
+$_['entry_geo_zone']     	 = 'Географічна зона:';
+$_['entry_status']       	 = 'Статус:';
+$_['entry_sort_order']   	 = 'Порядок сортування:';
 
 // Error
-$_['error_permission']       = 'У Вас нет прав для управления этим модулем!';
-$_['error_bank']             = 'Это поле обязательно для заполнения!';
+$_['error_permission']   	 = 'У Вас немає прав для управління цим модулем!';
+$_['error_bank']             = 'Це поле обов’язкове для заповнення!';
 ?>
