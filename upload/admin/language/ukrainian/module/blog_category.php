1b8acde70d3040e912cd072d0fd5bfd8d77f0529
ocshop <info@vilkov.info>
2015-05-08 03:37:24 +0300

    admin\blog_category.php

--- upload/admin/language/ukrainian/module/blog_category.php
+++ upload/admin/language/ukrainian/module/blog_category.php
@@ -1,22 +1,27 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']       = 'Категории Блога'; 
+$_['heading_title']       = 'Категорії Блогу'; 
 
 // Text
-$_['text_module']         = 'Модули';
-$_['text_success']        = 'Модуль категорий блога успешно обновлен!';
-$_['text_content_top']    = 'Верх страницы';
-$_['text_content_bottom'] = 'Низ страницы';
-$_['text_column_left']    = 'Левая колонка';
-$_['text_column_right']   = 'Правая колонка';
+$_['text_module']         = 'Модулі';
+$_['text_success']        = 'Модуль успішно оновлено!';
+$_['text_content_top']    = 'Верх сторінки';
+$_['text_content_bottom'] = 'Низ сторінки';
+$_['text_column_left']    = 'Ліва колонка';
+$_['text_column_right']   = 'Права колонка';
 
 // Entry
 $_['entry_layout']        = 'Схема:';
-$_['entry_position']      = 'Расположение:';
-$_['entry_count']    	  = 'Счетчик записей:';
+$_['entry_position']      = 'Розташування:';
+$_['entry_count']    	  = 'Лічильник записів:';
 $_['entry_status']        = 'Статус:';
-$_['entry_sort_order']    = 'Порядок сортировки:';
+$_['entry_sort_order']    = 'Порядок сортування:';
 
 // Error
-$_['error_permission']    = 'У Вас нет прав для изменения модуля категорий!';
+$_['error_permission']    = 'У Вас немає прав для зміни модуля!';
 ?>
