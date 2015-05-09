08dfaf5032b3bc409409ed225bbac83b42309f01
ocshop <info@vilkov.info>
2015-05-09 17:20:40 +0300

    admin\affiliate.php

--- upload/admin/language/ukrainian/sale/affiliate.php
+++ upload/admin/language/ukrainian/sale/affiliate.php
@@ -1,71 +1,76 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']             = 'Партнерская программа';
+$_['heading_title']             = 'Партнерська програма';
 
 // Text
-$_['text_success']              = 'Настройки успешно обновлены!';
-$_['text_approved']             = 'Вы одобрили %s аккаунтов!';
-$_['text_wait']                 = 'Пожалуйста, подождите!';
+$_['text_success']              = 'Налаштування успішно оновлені!';
+$_['text_approved']             = 'Ви схвалили %s акаунтів!';
+$_['text_wait']                 = 'Будь ласка зачекайте!';
 $_['text_balance']              = 'Баланс:';
 $_['text_cheque']               = 'Чек';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Банковкий перевод';
+$_['text_bank']                 = 'Банківських переказ';
 
 // Column
-$_['column_name']               = 'Имя партнера';
+$_['column_name']               = 'Ім’я партнера';
 $_['column_email']              = 'E-Mail';
-$_['column_code']               = 'Код отслеживания';
+$_['column_code']               = 'Код відстеження';
 $_['column_balance']            = 'Баланс';
 $_['column_status']             = 'Статус';
-$_['column_approved']           = 'Утверждено';
-$_['column_date_added']         = 'Дата добавления';
-$_['column_description']        = 'Описание';
-$_['column_amount']             = 'Сумма';
-$_['column_action']             = 'Действие';
+$_['column_approved']           = 'Затверджено';
+$_['column_date_added']         = 'Дата додавання';
+$_['column_description']        = 'Опис';
+$_['column_amount']             = 'Сума';
+$_['column_action']             = 'Дія';
 
 // Entry
-$_['entry_firstname']           = 'Имя:';
-$_['entry_lastname']            = 'Фамилия:';
+$_['entry_firstname']           = 'Ім’я:';
+$_['entry_lastname']            = 'Прізвище:';
 $_['entry_email']               = 'E-Mail:';
 $_['entry_telephone']           = 'Телефон:';
 $_['entry_fax']                 = 'Факс:';
 $_['entry_status']              = 'Статус:';
 $_['entry_password']            = 'Пароль:';
-$_['entry_confirm']             = 'Подтвердите пароль:';
-$_['entry_company']             = 'Компания:';
-$_['entry_address_1']           = 'Адрес 1:';
-$_['entry_address_2']           = 'Адрес 2:';
-$_['entry_city']                = 'Город:';
-$_['entry_postcode']            = 'Индекс:';
-$_['entry_country']             = 'Город:';
-$_['entry_zone']                = 'Регион / Область:';
-$_['entry_code']                = 'Код отслеживания:<span class="help">Этот код используется для отслеживания рефералов.</span>';
-$_['entry_commission']          = 'Вознаграждение (%):<span class="help">Процент, которые партнер получает за каждый заказ.</span>';
-$_['entry_tax']                 = 'ИНН:';
-$_['entry_payment']             = 'Способ оплаты:';
-$_['entry_cheque']              = 'Имя получателся чека:';
+$_['entry_confirm']             = 'Підтвердіть пароль:';
+$_['entry_company']             = 'Компанія:';
+$_['entry_address_1']           = 'Адреса 1:';
+$_['entry_address_2']           = 'Адреса 2:';
+$_['entry_city']                = 'Місто:';
+$_['entry_postcode']            = 'Індекс:';
+$_['entry_country']             = 'Місто:';
+$_['entry_zone']                = 'Регіон / Область:';
+$_['entry_code']                = 'Код відстеження:<span class="help">Цей код використовується для відстеження рефералів.</span>';
+$_['entry_commission']          = 'Винагорода (%):<span class="help">Відсоток, який партнер отримує за кожне замовлення.</span>';
+$_['entry_tax']                 = 'ІПН:';
+$_['entry_payment']             = 'Спосіб оплати:';
+$_['entry_cheque']              = 'Ім’я одержувача чека:';
 $_['entry_paypal']              = 'PayPal Email аккаунта:';
-$_['entry_bank_name']           = 'Название банка:';
-$_['entry_bank_branch_number']  = 'ABA/BSB номер (Номер отделения):';
+$_['entry_bank_name']           = 'Назва банку:';
+$_['entry_bank_branch_number']  = 'ABA/BSB номер (Номер відділення):';
 $_['entry_bank_swift_code']     = 'SWIFT Код:';
-$_['entry_bank_account_name']   = 'Имя счета:';
-$_['entry_bank_account_number'] = 'Номер счета:';
-$_['entry_amount']              = 'Сумма:';
-$_['entry_description']         = 'Описание:';
+$_['entry_bank_account_name']   = 'Ім’я рахунка:';
+$_['entry_bank_account_number'] = 'Номер рахунку:';
+$_['entry_amount']              = 'Сума:';
+$_['entry_description']         = 'Опис:';
 
 // Error
-$_['error_permission']          = 'У Вас нет прав для управления партнерами!';
-$_['error_exists']              = 'E-Mail адрес уже зарегистрирован!';
-$_['error_firstname']           = 'Имя должно быть от 1 до 32 символов!';
-$_['error_lastname']            = 'Фамилия должна быть от 1 до 32 символов!';
-$_['error_email']               = 'E-Mail адрес введен неверно!';
-$_['error_telephone']           = 'Номер телефона должен быть от 3 до 32 символов!';
-$_['error_password']            = 'Пароль должен быть от 4 до 20 символов!';
-$_['error_confirm']             = 'Пароли не совпадают!';
-$_['error_address_1']           = 'Адрес должен быть от 3 до 128 символов!';
-$_['error_city']                = 'Город должен быть от 2 до 128 символов!';
-$_['error_postcode']            = 'Индекс должен быть от 2 до 10 символов для этой страны!';
-$_['error_country']             = 'Пожалуйста, выберите страну!';
-$_['error_zone']                = 'Пожалуйста, выберите регион / область!';
-$_['error_code']                = 'Введите код отслеживания!';
+$_['error_permission']          = 'У Вас немає прав для управління партнерами!';
+$_['error_exists']              = 'E-Mail адреса вже зареєстрована!';
+$_['error_firstname']           = 'Ім’я повинно бути від 1 до 32 символів!';
+$_['error_lastname']            = 'Прізвище повинно бути від 1 до 32 символів!';
+$_['error_email']               = 'E-Mail адреса введено невірно!';
+$_['error_telephone']           = 'Номер телефону повинен бути від 3 до 32 символів!';
+$_['error_password']            = 'Пароль повинен бути від 4 до 20 символів!';
+$_['error_confirm']             = 'Паролі не співпадають!';
+$_['error_address_1']           = 'Адреса повинна бути від 3 до 128 символів!';
+$_['error_city']                = 'Місто має бути від 2 до 128 символів!';
+$_['error_postcode']            = 'Індекс повинен бути від 2 до 10 символів для цієї країни!';
+$_['error_country']             = 'Будь ласка, виберіть країну!';
+$_['error_zone']                = 'Будь ласка, виберіть регіон / область!';
+$_['error_code']                = 'Введіть код відстеження!';
 ?>
