57a514db693d4eb1aaad088edc0ae86db7076944
ocshop <info@vilkov.info>
2015-05-09 16:20:33 +0300

    admin/product_viewed.php

--- upload/admin/language/ukrainian/report/product_viewed.php
+++ upload/admin/language/ukrainian/report/product_viewed.php
@@ -1,13 +1,18 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']  = 'Отчет по просмотренным товарам';
+$_['heading_title']  		= 'Звіт по переглянутих товарах';
 
 // Text
-$_['text_success']   = 'Список просмотренных товаров очищен!';
+$_['text_success']   		= 'Список переглянутих товарів очищений!';
 
 // Column
-$_['column_name']    = 'Название товара';
-$_['column_model']   = 'Модель';
-$_['column_viewed']  = 'Просмотров';
-$_['column_percent'] = 'Процент';
+$_['column_name']       	= 'Назва товару';
+$_['column_model']      	= 'Модель';
+$_['column_viewed']  		= 'Переглядів';
+$_['column_percent'] 		= 'Відсоток';
 ?>
