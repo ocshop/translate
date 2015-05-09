1c6bd1d8d063e13c977dd1659dc2f47e3693f755
ocshop <info@vilkov.info>
2015-05-09 16:23:02 +0300

    admin\sale_coupon.php

--- upload/admin/language/ukrainian/report/sale_coupon.php
+++ upload/admin/language/ukrainian/report/sale_coupon.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Отчет по купонам';
+$_['heading_title']    		= 'Звіт по купонах';
 
 // Column
-$_['column_name']      = 'Имя купона';
-$_['column_code']      = 'Код';
-$_['column_orders']    = 'Заказы';
-$_['column_total']     = 'Итого';
-$_['column_action']    = 'Действие';
+$_['column_name']      		= 'Назва купона';
+$_['column_code']      		= 'Код';
+$_['column_orders']    		= 'Замовлення';
+$_['column_total']      	= 'Разом';
+$_['column_action']     	= 'Дія';
 
 // Entry
-$_['entry_date_start'] = 'Дата начала:';
-$_['entry_date_end']   = 'Дата окончания:';
+$_['entry_date_start']  	= 'Дата початку:';
+$_['entry_date_end']    	= 'Дата закінчення:';
 ?>
