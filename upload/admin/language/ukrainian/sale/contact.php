02eac4a87546eedb96301e97a4ba050c2ccc6eb7
ocshop <info@vilkov.info>
2015-05-09 17:29:56 +0300

    admin\contact.php

--- upload/admin/language/ukrainian/sale/contact.php
+++ upload/admin/language/ukrainian/sale/contact.php
@@ -1,31 +1,36 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    		= 'Почта';
+$_['heading_title']    		= 'Пошта';
 
 // Text
-$_['text_success']     		= 'Ваше сообщение отправлено!';
-$_['text_sent']             = 'Ваше сообщение было успешно отправлено %s из %s получателям!';
-$_['text_default']     		= 'По умолчанию';
-$_['text_newsletter']  		= 'Подписчики на новости';
-$_['text_customer_all']    	= 'Все покупатели';
-$_['text_customer_group']  	= 'Группа покупателей';
-$_['text_customer']        	= 'Покупатели';
-$_['text_affiliate_all']   	= 'Все партнёры';
-$_['text_affiliate']       	= 'Партнёры';
-$_['text_product']         	= 'Товары';
+$_['text_success']     		= 'Ваше повідомлення надіслано!';
+$_['text_sent']             = 'Ваше повідомлення було успішно надіслано %s з %s одержувачів!';
+$_['text_default']     		= 'За замовчуванням';
+$_['text_newsletter']  		= 'Підписчики на новини';
+$_['text_customer_all']    	= 'Всі покупці';
+$_['text_customer_group']  	= 'Група покупців';
+$_['text_customer']        	= 'Покупці';
+$_['text_affiliate_all']   	= 'Всі партнери';
+$_['text_affiliate']       	= 'Партнери';
+$_['text_product']         	= 'Товари';
 
 // Entry
-$_['entry_store']      		= 'От:';
+$_['entry_store']      		= 'Від:';
 $_['entry_to']         		= 'Кому:';
-$_['entry_customer_group'] 	= 'Группа покупателя:';
-$_['entry_customer']       	= 'Покупатель:<br /><span class="help">Автодополнение</span>';
-$_['entry_affiliate']      	= 'Партнёр:<br /><span class="help">Автодополнение</span>';
-$_['entry_product']        	= 'Товары:<br /><span class="help">Отправить покупателям, которые уже заказали товары из списка. (Автодополнение)</span>';
+$_['entry_customer_group'] 	= 'Група покупця:';
+$_['entry_customer']       	= 'Покупець:<br /><span class="help">Автодоповнення</span>';
+$_['entry_affiliate']      	= 'Партнер:<br /><span class="help">Автодоповнення</span>';
+$_['entry_product']        	= 'Товари:<br /><span class="help">Відправити покупцям, які вже замовили товари зі списку. (Автодоповнення)</span>';
 $_['entry_subject']    		= 'Тема:';
-$_['entry_message']    		= 'Сообщение:';
+$_['entry_message']    		= 'Повідомлення:';
 
 // Error
-$_['error_permission'] 		= 'У Вас нет прав для отправки сообщений!';
-$_['error_subject']    		= 'Необходима тема письма!';
-$_['error_message']    		= 'Необходим текст сообщения!';
+$_['error_permission'] 		= 'У Вас немає прав для відправки повідомлень!';
+$_['error_subject']    		= 'Необхідна тема листа!';
+$_['error_message']    		= 'Необхідний текст повідомлення!';
 ?>
