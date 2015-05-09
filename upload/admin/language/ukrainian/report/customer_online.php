b6870ca060422603d1b484ef39c0380665d7a938
ocshop <info@vilkov.info>
2015-05-09 16:10:06 +0300

    admin\customer_online.php

--- upload/admin/language/ukrainian/report/customer_online.php
+++ upload/admin/language/ukrainian/report/customer_online.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']     = 'Отчёт об онлайн пользователях';
+$_['heading_title']     = 'Звіт про онлайн користувачів';
 
 // Text 
-$_['text_guest']        = 'Гость';
+$_['text_guest']        = 'Гість';
  
 // Column
 $_['column_ip']         = 'IP';
-$_['column_customer']   = 'покупатель';
-$_['column_url']        = 'Последняя посещенная страница';
-$_['column_referer']    = 'Откуда пришёл';
-$_['column_date_added'] = 'Последний клик';
-$_['column_action']     = 'Действие';
+$_['column_customer']   = 'Покупець';
+$_['column_url']        = 'Остання відвідана сторінка';
+$_['column_referer']    = 'Звідки прийшов';
+$_['column_date_added'] = 'Останній клік';
+$_['column_action']     = 'Дія';
 ?>
