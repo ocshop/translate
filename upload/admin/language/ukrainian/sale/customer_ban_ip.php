c855006788942eebdefbd3f1b5660e257a704479
ocshop <info@vilkov.info>
2015-05-09 17:58:13 +0300

    admin\customer_ban_ip.php

--- upload/admin/language/ukrainian/sale/customer_ban_ip.php
+++ upload/admin/language/ukrainian/sale/customer_ban_ip.php
@@ -1,19 +1,24 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Запрещенные IP покупателей';
+$_['heading_title']    = 'Заборонені IP покупців';
 
 // Text
-$_['text_success']     = 'Изменения внесены успешно!';
+$_['text_success']     = 'Зміни внесені успішно!';
 
 // Column
 $_['column_ip']        = 'IP';
-$_['column_customer']  = 'Покупатели';
-$_['column_action']    = 'Действие';
+$_['column_customer']  = 'Покупці';
+$_['column_action']    = 'Дія';
 
 // Entry
 $_['entry_ip']         = 'IP:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для изменения черного списка IP-адресов покупателей!';
-$_['error_ip']         = 'IP должен быть от 1 до 15 символов!';
+$_['error_permission'] = 'У Вас немає прав для зміни чорного списку IP-адрес покупців!';
+$_['error_ip']         = 'IP повинен бути від 1 до 15 символів!';
 ?>
