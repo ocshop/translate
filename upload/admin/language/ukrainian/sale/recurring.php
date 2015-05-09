9621a2f4dc5730991b6f87ba1c5016ed168ca219
ocshop <info@vilkov.info>
2015-05-09 20:16:49 +0300

    admin\recurring.php

--- upload/admin/language/ukrainian/sale/recurring.php
+++ upload/admin/language/ukrainian/sale/recurring.php
@@ -1,51 +1,57 @@
 <?php
 
-$_['heading_title'] = 'Платежные профили';
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
 
-$_['text_success'] = 'Платежные профили обновлены!';
-$_['text_payment_profiles'] = 'Платежные профили';
-$_['text_status_active'] = 'Активен';
-$_['text_status_inactive'] = 'Неактивен';
-$_['text_status_cancelled'] = 'Аннулирован';
-$_['text_status_suspended'] = 'Заморожен';
-$_['text_status_expired'] = 'Истечен';
-$_['text_status_pending'] = 'Ожидает';
-$_['text_transactions'] = 'Операция';
-$_['text_return'] = 'Возврат';
-$_['text_cancel'] = 'Отмена';
-$_['text_filter'] = 'Фильтр';
+// Heading
+$_['heading_title'] 						= 'Платіжні профілі';
 
-$_['text_cancel_confirm'] = "Profile's cancelation cannot be undone! Are you sure want to do this?";
+// Text
+$_['text_success'] 							= 'Платіжні профілі оновлені!';
+$_['text_payment_profiles'] 				= 'Платіжні профілі';
+$_['text_status_active'] 					= 'Активний';
+$_['text_status_inactive'] 					= 'Неактивен';
+$_['text_status_cancelled'] 				= 'Анульовано';
+$_['text_status_suspended'] 				= 'Заморожений';
+$_['text_status_expired'] 					= 'Закінчився';
+$_['text_status_pending'] 					= 'Чекає';
+$_['text_transactions'] 					= 'Операція';
+$_['text_return'] 							= 'Повернення';
+$_['text_cancel'] 							= 'Скасування';
+$_['text_filter'] 							= 'Фільтр';
+$_['text_cancel_confirm'] 					= 'Скасування профіль не може бути скасовано! Ви впевнені, що хочете це зробити?';
+$_['text_transaction_created'] 				= 'Створено';
+$_['text_transaction_payment'] 				= 'Платіж';
+$_['text_transaction_outstanding_payment'] 	= 'Несплачений внесок';
+$_['text_transaction_skipped'] 				= 'Платіж пропущений';
+$_['text_transaction_failed'] 				= 'Проблема з оплатою';
+$_['text_transaction_cancelled'] 			= 'Скасовано';
+$_['text_transaction_suspended'] 			= 'Заморожений';
+$_['text_transaction_suspended_failed'] 	= 'Заморожена оплата';
+$_['text_transaction_outstanding_failed'] 	= 'Платіж не пройшов';
+$_['text_transaction_expired'] 				= 'Закінчився';
 
-$_['text_transaction_created'] = 'Создан';
-$_['text_transaction_payment'] = 'Платеж';
-$_['text_transaction_outstanding_payment'] = 'Неуплаченный взнос';
-$_['text_transaction_skipped'] = 'Платеж пропущен';
-$_['text_transaction_failed'] = 'Проблема с оплатой';
-$_['text_transaction_cancelled'] = 'Отменен';
-$_['text_transaction_suspended'] = 'Заморожен';
-$_['text_transaction_suspended_failed'] = 'Suspended from failed payment';
-$_['text_transaction_outstanding_failed'] = 'Outstanding payment failed';
-$_['text_transaction_expired'] = 'Истек';
+// Entry
+$_['entry_cancel_payment'] 					= 'Скасування оплати:';
+$_['entry_order_recurring'] 				= '№:';
+$_['entry_order_id'] 						= '№ замовлення:';
+$_['entry_payment_reference'] 				= 'Метод оплати';
+$_['entry_customer'] 						= 'Клієнт';
+$_['entry_date_created'] 					= 'Дата створення';
+$_['entry_status'] 							= 'Статус';
+$_['entry_type'] 							= 'Тип';
+$_['entry_action'] 							= 'Дія';
+$_['entry_email'] 							= 'E-Mail';
+$_['entry_profile_description'] 			= "Опис профілю";
+$_['entry_product'] 						= "Продукт:";
+$_['entry_quantity'] 						= "Кількість:";
+$_['entry_amount'] 							= "Підсумок:";
+$_['entry_profile'] 						= "Профіль:";
+$_['entry_payment_type'] 					= "Метод оплати:";
 
-$_['entry_cancel_payment'] = 'Отмена оплаты:';
-$_['entry_order_recurring'] = '№:';
-$_['entry_order_id'] = '№ заказа:';
-$_['entry_payment_reference'] = 'Метод оплаты';
-$_['entry_customer'] = 'Клиент';
-$_['entry_date_created'] = 'Дата создания';
-$_['entry_status'] = 'Статус';
-$_['entry_type'] = 'Тип';
-$_['entry_action'] = 'Действие';
-$_['entry_email'] = 'Email';
-$_['entry_profile_description'] = "Описание профиля";
-$_['entry_product'] = "Продукт:";
-$_['entry_quantity'] = "Количество:";
-$_['entry_amount'] = "Итог:";
-$_['entry_profile'] = "Профиль:";
-$_['entry_payment_type'] = "Метод оплаты:";
-
-$_['error_not_cancelled'] = 'Ошибка: %s';
-$_['error_not_found'] = 'Нельзя отменить профиль';
-$_['success_cancelled'] = 'Периодический платеж был отменен';
+// Error
+$_['error_not_cancelled'] 					= 'Помилка: %s';
+$_['error_not_found'] 						= 'Не можна скасувати профіль';
+$_['success_cancelled'] 					= 'Періодичний платіж було скасовано';
 ?>
