be04863ff1da405236958cd1b93b8138bcd0d70f
ocshop <info@vilkov.info>
2015-05-10 05:06:12 +0300

    admin\reward.php

--- upload/admin/language/ukrainian/total/reward.php
+++ upload/admin/language/ukrainian/total/reward.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Бонусные баллы';
+$_['heading_title']    = 'Бонусні бали';
 
 // Text
-$_['text_total']       = 'Общая сумма заказа';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_total']       = 'Загальна сума замовлення';
+$_['text_success']     = 'Налаштування модуля оновлені!';
 
 // Entry
 $_['entry_status']     = 'Статус:';
-$_['entry_sort_order'] = 'Порядок сортировки:';
+$_['entry_sort_order'] = 'Порядок сортування:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления бонусными баллами!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
