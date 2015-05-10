bf7ce62f9b64a1afa1bf9d7f4cb584619243b6eb
ocshop <info@vilkov.info>
2015-05-10 05:07:21 +0300

    admin\shipping.php

--- upload/admin/language/ukrainian/total/shipping.php
+++ upload/admin/language/ukrainian/total/shipping.php
@@ -1,16 +1,21 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']    = 'Доставка';
 
 // Text
-$_['text_total']       = 'Общая сумма заказа';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_total']       = 'Загальна сума замовлення';
+$_['text_success']     = 'Налаштування модуля оновлені!';
 
 // Entry
-$_['entry_estimator']  = 'Оценка стоимости доставки:';
+$_['entry_estimator']  = 'Оцінка вартості доставки:';
 $_['entry_status']     = 'Статус:';
-$_['entry_sort_order'] = 'Порядок сортировки:';
+$_['entry_sort_order'] = 'Порядок сортування:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления доставкой!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
