af959337e8bcb36cb547c6ae1fa82ff75ea4e7bf
ocshop <info@vilkov.info>
2015-05-10 05:02:28 +0300

    admin\credit.php

--- upload/admin/language/ukrainian/total/credit.php
+++ upload/admin/language/ukrainian/total/credit.php
@@ -1,15 +1,20 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Кредит магазина';
+$_['heading_title']    = 'Кредит магазину';
 
 // Text
-$_['text_total']       = 'Общая сумма заказа';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_total']       = 'Загальна сума замовлення';
+$_['text_success']     = 'Налаштування модуля оновлені!';
 
 // Entry
 $_['entry_status']     = 'Статус:';
-$_['entry_sort_order'] = 'Порядок сортировки:';
+$_['entry_sort_order'] = 'Порядок сортування:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления кредитами магазина!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
