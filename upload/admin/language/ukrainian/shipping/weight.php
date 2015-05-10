d70086710538e0e8e0425768963fd5887ca31f7d
ocshop <info@vilkov.info>
2015-05-10 04:31:59 +0300

    admin\weight.php

--- upload/admin/language/ukrainian/shipping/weight.php
+++ upload/admin/language/ukrainian/shipping/weight.php
@@ -1,18 +1,23 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']      = 'Доставка в зависимости от веса';
+$_['heading_title']       = 'Доставка в залежності від ваги';
 
 // Text
-$_['text_shipping']      = 'Доставка';
-$_['text_success']       = 'Настройки модуля обновлены!';
+$_['text_shipping']    	  = 'Доставка';
+$_['text_success']     	  = 'Налаштування модуля оновлені!';
 
 // Entry
-$_['entry_rate']         = 'Расценки:<br /><span class="help">Например: 5:10.00,7:12.00 Вес:Цена,Вес:Цена, и т.д.</span>';
-$_['entry_tax_class']    = 'Класс налога:';
-$_['entry_geo_zone']     = 'Географическая зона:';
+$_['entry_rate']         = 'Тарифи доставки:<br /><span class="help">Наприклад: 5:10.00,7:12.00 Вага:Ціна, Вага:Ціна, і т.д.</span>';
+$_['entry_tax_class']    = 'Податковий клас:';
+$_['entry_geo_zone']     = 'Географічна зона:';
 $_['entry_status']       = 'Статус:';
-$_['entry_sort_order']   = 'Порядок сортировки:';
+$_['entry_sort_order']   = 'Порядок сортування:';
 
 // Error
-$_['error_permission']   = 'У Вас нет прав для управления этим модулем!';
+$_['error_permission']   = 'У Вас немає прав для управління цим модулем!';
 ?>
