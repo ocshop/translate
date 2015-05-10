5f25c80ad4f21385596bb09a1b003016f69f62e2
ocshop <info@vilkov.info>
2015-05-10 04:22:50 +0300

    admin\free.php

--- upload/admin/language/ukrainian/shipping/free.php
+++ upload/admin/language/ukrainian/shipping/free.php
@@ -1,17 +1,22 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Бесплатная доставка';
+$_['heading_title']    = 'Безкоштовна доставка';
 
 // Text
-$_['text_shipping']    = 'Бесплатная доставка';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_shipping']    = 'Безкоштовна доставка';
+$_['text_success']     = 'Налаштування модуля оновлені!';
 
 // Entry
-$_['entry_total']      = 'Порог стоимости:<br /><span class="help">Минимальная сумма, необходимая для того, чтобы бесплатная доставка стала доступна.</span>';
-$_['entry_geo_zone']   = 'Географическая зона:';
+$_['entry_total']      = 'Поріг вартості:<br /><span class="help">Мінімальна сума, необхідна для того, щоб безкоштовна доставка стала доступна.</span>';
+$_['entry_geo_zone']   = 'Географічна зона:';
 $_['entry_status']     = 'Статус:';
-$_['entry_sort_order'] = 'Порядок сортировки:';
+$_['entry_sort_order'] = 'Порядок сортування:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
