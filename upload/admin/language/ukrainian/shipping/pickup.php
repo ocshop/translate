3e0144d5fc9d70f90595d8419491245398a92556
ocshop <info@vilkov.info>
2015-05-10 04:29:18 +0300

    admin\pickup.php

--- upload/admin/language/ukrainian/shipping/pickup.php
+++ upload/admin/language/ukrainian/shipping/pickup.php
@@ -1,16 +1,21 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Самовывоз';
+$_['heading_title']    = 'Самовивіз';
 
 // Text
 $_['text_shipping']    = 'Доставка';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_success']     = 'Налаштування модуля оновлені!';
 
 // Entry
-$_['entry_geo_zone']   = 'Географическая зона:';
+$_['entry_geo_zone']   = 'Географічна зона:';
 $_['entry_status']     = 'Статус:';
-$_['entry_sort_order'] = 'Порядок сортировки:';
+$_['entry_sort_order'] = 'Порядок сортування:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
