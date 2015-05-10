6a12363aef36d3ccfa6bd53f3ae27de4172d3d4a
ocshop <info@vilkov.info>
2015-05-10 04:12:34 +0300

    admin\flat.php

--- upload/admin/language/ukrainian/shipping/flat.php
+++ upload/admin/language/ukrainian/shipping/flat.php
@@ -1,18 +1,23 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Фиксированная стоимость доставки';
+$_['heading_title']    = 'Фіксована вартість доставки';
 
 // Text
-$_['text_shipping']    = 'Фиксированная стоимость доставки';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_shipping']    = 'Фіксована вартість доставки';
+$_['text_success']     = 'Налаштування модуля оновлені!';
 
 // Entry
-$_['entry_cost']       = 'Стоимость:';
-$_['entry_tax_class']  = 'Класс налога:';
-$_['entry_geo_zone']   = 'Географическая зона:';
+$_['entry_cost']       = 'Вартість:';
+$_['entry_tax_class']  = 'Податковий клас:';
+$_['entry_geo_zone']   = 'Географічна зона:';
 $_['entry_status']     = 'Статус:';
-$_['entry_sort_order'] = 'Порядок сортировки:';
+$_['entry_sort_order'] = 'Порядок сортування:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
