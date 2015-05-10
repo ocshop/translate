b80228bf4029db470b0d9ced450c71f1417c0b5a
ocshop <info@vilkov.info>
2015-05-10 04:48:25 +0300

    admin\seomanager.php

--- upload/admin/language/ukrainian/tool/seomanager.php
+++ upload/admin/language/ukrainian/tool/seomanager.php
@@ -1,19 +1,24 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
 $_['heading_title']     	= 'SEO Менеджер';
 
 // Text
-$_['text_success']      	= 'SEO URL успешно обновлены!';
-$_['text_success_clear'] 	= 'Кэш SEO URL успешно сброшен!';
-$_['text_default']      	= 'По умолчанию';
-$_['text_module']			= 'Модули';
-$_['button_clear_cache'] 	= 'Сбросить кэш';
+$_['text_success']      	= 'SEO URL успішно оновлені!';
+$_['text_success_clear'] 	= 'Кеш SEO URL успішно скинутий!';
+$_['text_default']      	= 'За замовчуванням';
+$_['text_module']			= 'Модулі';
+$_['button_clear_cache'] 	= 'Скинути кеш';
 
 // Column
-$_['column_query']       	= 'Ссылка';
+$_['column_query']       	= 'URL';
 $_['column_keyword']       	= 'SEO URL';
-$_['column_action']     	= 'Действие';
+$_['column_action']     	= 'Дія';
 
 // Error
-$_['error_permission']  	= 'У Вас недостаточно прав для изменения SEO Менеджер!';
+$_['error_permission'] 		= 'У Вас немає прав для управління цим модулем!';
 ?>
