6d93fe82a4b3883c3162c47773da940e5c374183
ocshop <info@vilkov.info>
2015-05-10 05:05:19 +0300

    admin\low_order_fee.php

--- upload/admin/language/ukrainian/total/low_order_fee.php
+++ upload/admin/language/ukrainian/total/low_order_fee.php
@@ -1,18 +1,23 @@
 <?php
+
+// Ukrainian Localization OCSHOP.CMS 1.5.6.4.1 http://ocshop.net
+// BLOG http://ocshop.info
+// FORUM http://forum.ocshop.info
+
 // Heading
-$_['heading_title']    = 'Доп. плата при минимальном заказе';
+$_['heading_title']    = 'Доп. плата при мінімальному замовленні';
 
 // Text
-$_['text_total']       = 'Общая сумма заказа';
-$_['text_success']     = 'Настройки модуля обновлены!';
+$_['text_total']       = 'Загальна сума замовлення';
+$_['text_success']     = 'Налаштування модуля оновлені!'
 
 // Entry
-$_['entry_total']      = 'Сумма заказа:';
+$_['entry_total']      = 'Сума замовлення:';
 $_['entry_fee']        = 'Плата:';
-$_['entry_tax_class']  = 'Налоги:';
+$_['entry_tax_class']  = 'Податковий клас:';
 $_['entry_status']     = 'Статус:';
-$_['entry_sort_order'] = 'Порядок сортировки:';
+$_['entry_sort_order'] = 'Порядок сортування:';
 
 // Error
-$_['error_permission'] = 'У Вас нет прав для управления этим модулем!';
+$_['error_permission'] = 'У Вас немає прав для управління цим модулем!';
 ?>
