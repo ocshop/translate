9d3a36b5850ff4e757e957737364f43538fb5e15
ocshop <info@vilkov.info>
2015-07-19 15:49:46 +0300

    account.php

--- upload/catalog/language/russian/affiliate/account.php
+++ upload/catalog/language/russian/affiliate/account.php
@@ -1,14 +1,21 @@
 <?php
+// *	@copyright	OCSHOP.CMS \ ocshop.net 2011 - 2015.
+// *	@demo	http://ocshop.net
+// *	@blog	http://ocshop.info
+// *	@forum	http://forum.ocshop.info
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
 // Heading
-$_['heading_title']        = 'My Affiliate Account';
+$_['heading_title']        = 'Партнерcкий раздел';
 
 // Text
-$_['text_account']         = 'Аккаунт';
-$_['text_my_account']      = 'My Affiliate Account';
-$_['text_my_tracking']     = 'My Tracking Information';
-$_['text_my_transactions'] = 'My Transactions';
-$_['text_edit']            = 'Edit your account information';
-$_['text_password']        = 'Change your password';
-$_['text_payment']         = 'Change your payment preferences';
-$_['text_tracking']        = 'Custom Affiliate Tracking Code';
-$_['text_transaction']     = 'View your transaction history';
+$_['text_account']         = 'Кабинет Партнера';
+$_['text_my_account']      = 'Моя учетная запись';
+$_['text_my_tracking']     = 'Мои рефералы';
+$_['text_my_transactions'] = 'История выплат';
+$_['text_edit']            = 'Редактировать учетную запись';
+$_['text_password']        = 'Изменить пароль';
+$_['text_payment']         = 'Изменить платежные реквизиты';
+$_['text_tracking']        = 'Реферальный код';
+$_['text_transaction']     = 'Посмотреть историю выплат';
