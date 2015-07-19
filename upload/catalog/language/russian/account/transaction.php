669fcbf1335ee328f41148b134c0ac2ffcd34808
ocshop <info@vilkov.info>
2015-07-19 15:45:58 +0300

    transaction.php

--- upload/catalog/language/russian/account/transaction.php
+++ upload/catalog/language/russian/account/transaction.php
@@ -1,14 +1,21 @@
 <?php
+// *	@copyright	OCSHOP.CMS \ ocshop.net 2011 - 2015.
+// *	@demo	http://ocshop.net
+// *	@blog	http://ocshop.info
+// *	@forum	http://forum.ocshop.info
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
 // Heading
-$_['heading_title']      = 'Your Transactions';
+$_['heading_title']      = 'История финансовых операций';
 
 // Column
 $_['column_date_added']  = 'Добавлено';
 $_['column_description'] = 'Описание';
-$_['column_amount']      = 'Amount (%s)';
+$_['column_amount']      = 'Сумма (%s)';
 
 // Text
-$_['text_account']       = 'Аккаунт';
-$_['text_transaction']   = 'Your Transactions';
-$_['text_total']         = 'Your current balance is:';
-$_['text_empty']         = 'You do not have any transactions!';
+$_['text_account']       = 'Личный Кабинет';
+$_['text_transaction']   = 'Финансовые операции';
+$_['text_total']         = 'Ваш текущий баланс:';
+$_['text_empty']         = 'Не было финансовых операций!';
