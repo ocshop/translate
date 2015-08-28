952a66c80ee2c253c06d1e3ef633f80e744ba9a6
ocshop <info@vilkov.info>
2015-08-29 02:39:07 +0300

    Дополнение перевода

--- upload/admin/language/ukrainian/captcha/basic_captcha.php
+++ upload/admin/language/ukrainian/captcha/basic_captcha.php
@@ -9,7 +9,7 @@
 $_['heading_title']    = 'Стандартная Капча';
 
 // Text
-$_['text_captcha']     = 'Капча';
+$_['text_captcha']     = 'Капча'; ua
 $_['text_success']	   = 'Настройки успешно изменены!';
 $_['text_edit']        = 'Настройки капчи';
 
---
+++ upload/catalog/language/ukrainian/captcha/google_captcha.php
@@ -0,0 +1,16 @@
+<?php
+// *	@copyright	OCSHOP.CMS \ ocshop.net 2011 - 2015.
+// *	@demo	http://ocshop.net
+// *	@blog	http://ocshop.info
+// *	@forum	http://forum.ocshop.info
+// *	@source		See SOURCE.txt for source and other copyright.
+// *	@license	GNU General Public License version 3; see LICENSE.txt
+
+// Heading
+$_['heading_title'] = 'Капча'; ua
+
+// Entry
+$_['entry_captcha'] = 'Введіть код в поле нижче'; ua
+
+// Error
+$_['error_captcha'] = 'Невірно введений код з картинки!'; ua
