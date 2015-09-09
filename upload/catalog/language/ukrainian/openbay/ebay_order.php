bb589abe4189bd15dec0d27cb9d129a0a9e9ad65
ocshop <info@vilkov.info>
2015-09-09 09:55:10 +0300

    Дополнение перевода

--- upload/catalog/language/ukrainian/openbay/ebay_order.php
+++ upload/catalog/language/ukrainian/openbay/ebay_order.php
@@ -7,8 +7,10 @@
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Text
-$_['text_total_shipping']		= 'Доставка';
-$_['text_total_discount']		= 'Скидка';
-$_['text_total_tax']			= 'Без НДС';
-$_['text_total_sub']			= 'Всього'; ua
-$_['text_total']				= 'Всього'; ua
+$_['text_total_shipping']		= 'Shipping';
+$_['text_total_discount']		= 'Discount';
+$_['text_total_tax']			= 'Tax';
+$_['text_total_sub']			= 'Sub-total';
+$_['text_total']				= 'Total';
+$_['text_smp_id']				= 'Selling Manager sale ID: ';
+$_['text_buyer']				= 'Buyer username: ';
