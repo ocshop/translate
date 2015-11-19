e7d2e113fae2bb7e384fc422aa4c8d30cc2c184a
ocshop <info@vilkov.info>
2015-11-19 11:42:22 +0200

    номер заказа на странице подтверждения

--- upload/catalog/language/ukrainian/checkout/success.php
+++ upload/catalog/language/ukrainian/checkout/success.php
@@ -7,7 +7,8 @@
 // *	@license	GNU General Public License version 3; see LICENSE.txt
 
 // Heading
-$_['heading_title']        = 'Ваше замовлення сформоване!'; ua
+$_['heading_title']          = 'Ваше замовлення #%s сформоване!'; ua
+$_['heading_title_customer'] = 'Ваше замовлення #%s сформоване!'; ua
 
 // Text
 $_['text_basket']          = 'Кошик покупок'; ua
